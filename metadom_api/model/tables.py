304799293196e1d761cb26c3fb223fd49c8370a8
laurensvdwiel <laurensvdwiel@gmail.com>
2017-05-23 15:02:14 +0200

    cleaned up comments

--- metadom_api/model/tables.py
+++ metadom_api/model/tables.py
@@ -7,18 +7,19 @@ Base = declarative_base()
 
 class Chromosome(Base):
     """
-    CREATE TABLE `chromosomes` (
-      `id` INTEGER NOT NULL AUTO_INCREMENT DEFAULT NULL,
-      `chromosome` CHAR(2) NULL DEFAULT NULL,
-      `position` INTEGER NULL DEFAULT NULL,
-      PRIMARY KEY (`id`)
-    );"""
+    Table: chromosomes
+    
+    Fields
+    id                        identifier
+    chromosome                chromosome '1'-'22', 'X' or 'Y'
+    position                  position in the chromosome
+    """
     
     __tablename__ = 'chromosomes'
     
     id = Column(Integer, primary_key=True)
-    chromosome = Column(String(2))                      # chromosome '1'-'22', 'X' or 'Y'
-    position = Column(Integer)                          # position in the chromosome
+    chromosome = Column(String(2))
+    position = Column(Integer)
     
     def __repr__(self):
         return "<Chromosome(chromosome='%s', position='%s')>" % (
@@ -27,28 +28,29 @@ class Chromosome(Base):
 
 class Mapping(Base):
     """
-    CREATE TABLE `mapping` (
-      `id` INTEGER NOT NULL AUTO_INCREMENT DEFAULT NULL,
-      `allele` CHAR(1) NULL DEFAULT NULL,
-      `codon` CHAR(3) NULL DEFAULT NULL,
-      `codon_allele_position` INTEGER NULL DEFAULT NULL,
-      `amino_acid_residue` CHAR(1) NULL DEFAULT 'X',
-      `cDNA_position` INTEGER NULL DEFAULT NULL,
-      `uniprot_position` INTEGER NULL DEFAULT NULL,
-      `pfam_consensus_position` INTEGER NULL DEFAULT NULL,
-      PRIMARY KEY (`id`)
-    );"""
+    Table: mapping
+    
+    Fields
+    id                        identifier
+    allele                    'A', 'T', 'C', 'G'
+    codon                     all triplet combinations of the alleles 
+    codon_allele_position     0, 1, 2, or None
+    amino_acid_residue        one of the 20 amino acids, * or None
+    cDNA_position             the position in the cDNA
+    uniprot_position          the position in the protein
+    pfam_consensus_position   position in the aligned Pfam domain or None
+    """
     
     __tablename__ = 'mapping'
     
     id = Column(Integer, primary_key=True)
-    allele = Column(String(1))                          # 'A', 'T', 'C', 'G'
-    codon = Column(String(3))                           # all triplet combinations of the alleles 
-    codon_allele_position = Column(Integer)             # 0, 1, 2, or None
-    amino_acid_residue = Column(String(1))              # one of the 20 amino acids, * or None
-    cDNA_position = Column(Integer)                     # the position in the cDNA
-    uniprot_position = Column(Integer)                  # the position in the protein
-    pfam_consensus_position = Column(Integer)           # position in the aligned Pfam domain or None
+    allele = Column(String(1))
+    codon = Column(String(3))
+    codon_allele_position = Column(Integer)
+    amino_acid_residue = Column(String(1))
+    cDNA_position = Column(Integer)
+    uniprot_position = Column(Integer)
+    pfam_consensus_position = Column(Integer)
     
     def __repr__(self):
         return "<Mapping(allele='%s', codon='%s', codon_allele_position='%s', amino_acid_residue='%s', cDNA_position='%s', uniprot_position='%s', pfam_consensus_position='%s')>" % (
@@ -57,27 +59,27 @@ class Mapping(Base):
     
 class Pfam(Base):
     """
-    CREATE TABLE `pfam` (
-      `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-      `pfam_id` VARCHAR(10) NOT NULL DEFAULT 'NULL',
-      `name` VARCHAR(255) NULL DEFAULT NULL,
-      `interpro_id` VARCHAR(12) NULL DEFAULT NULL,
-      `uniprot_ac` VARCHAR(12) NULL DEFAULT NULL,
-      `uniprot_start` INTEGER NOT NULL DEFAULT 0,
-      `uniprot_stop` INTEGER NOT NULL DEFAULT 0,
-      PRIMARY KEY (`id`)
-    );
+    Table: pfam
+    
+    Fields
+    id                        identifier
+    pfam_id                   Pfam identifier code 'PF#####'
+    name                      Name of the Pfam domain
+    interpro_id               Interpro identifier  
+    uniprot_ac                uniprot accession code
+    uniprot_start             0 <= uniprot_start < uniprot_stop
+    uniprot_stop              uniprot_stop <= uniprot_end
     """
     
     __tablename__ = 'pfam'
     
     id = Column(Integer, primary_key=True)
-    pfam_id = Column(String(12))                        # Pfam identifier code 'PF#####'
-    name = Column(String)                               # Name of the Pfam domain
-    interpro_id = Column(String(12))                    # Interpro identifier  
-    uniprot_ac = Column(String(12))                     # uniprot accession code
-    uniprot_start = Column(Integer)                     # 0 <= uniprot_start < uniprot_stop
-    uniprot_stop = Column(Integer)                      # uniprot_stop <= uniprot_end
+    pfam_id = Column(String(12))
+    name = Column(String)
+    interpro_id = Column(String(12))  
+    uniprot_ac = Column(String(12))
+    uniprot_start = Column(Integer)
+    uniprot_stop = Column(Integer)
     
     
     def __repr__(self):
@@ -86,15 +88,14 @@ class Pfam(Base):
 
 class Protein(Base):
     """
-    CREATE TABLE `protein` (
-      `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-      `uniprot_ac` VARCHAR(12) NOT NULL DEFAULT 'NULL',
-      `uniprot_name` VARCHAR(20) NULL DEFAULT NULL,
-      `source` ENUM ('swissprot','uniprot'),
-      PRIMARY KEY (`id`),
-      UNIQUE KEY (`uniprot_ac`),
-    KEY (`uniprot_ac`)
-    );"""
+    Table: protein
+    
+    Fields
+    id                        identifier
+    uniprot_ac                uniprot accession code
+    uniprot_name              uniprot name
+    source                    'swissprot' or 'uniprot'
+    """
     
     class ProteinSource(enum.Enum):
         uniprot = 'uniprot'
@@ -114,18 +115,18 @@ class Protein(Base):
 
 class Gene(Base):
     """
-    CREATE TABLE `gene` (
-      `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-      `strand` ENUM ('+', '-'),
-      `gene_name` VARCHAR(50) NOT NULL DEFAULT '',
-      `gencode_transcription_id` VARCHAR(50) NOT NULL DEFAULT '',
-      `gencode_translation_name` VARCHAR(50) NOT NULL DEFAULT '',
-      `gencode_gene_id` VARCHAR(50) NOT NULL DEFAULT '',
-      `havana_gene_id` VARCHAR(50) NOT NULL DEFAULT '',
-      `havana_translation_id` VARCHAR(50) NOT NULL DEFAULT '',
-      PRIMARY KEY (`id`),
-      UNIQUE KEY (`gencode_transcription_id`, `gencode_translation_name`, `havana_translation_id`)
-    );"""
+    Table: gene
+    
+    Fields
+    id                        identifier
+    strand                    '+' or '-'
+    gene_name                 Conventional (HAVANA) name of the gene
+    gencode_transcription_id  e.g. ENST####...
+    gencode_translation_name  e.g. BRCA1-###
+    gencode_gene_id           e.g. ENSG####...
+    havana_gene_id            e.g. OTTHUMG#####....
+    havana_translation_id     e.g. OTTHUMT#####....
+    """
     
     class Strand(enum.Enum):
         plus = '+'
