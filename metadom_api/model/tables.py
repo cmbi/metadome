19c072952a371e5766cb776f8231c73d65241a13
laurensvdwiel <laurensvdwiel@gmail.com>
2017-05-24 12:28:41 +0200

    added constraints on the pfam_domains table and changed the test data, to test instances

--- metadom_api/model/tables.py
+++ metadom_api/model/tables.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Enum
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
+from sqlalchemy.sql.schema import UniqueConstraint
 
 Base = declarative_base()
 
@@ -120,6 +121,10 @@ class Pfam(Base):
     protein = relationship("Protein", back_populates="pfam_domains")
     mappings = relationship('Mapping', back_populates="pfam_domain")
     
+    # Constraints
+    __table_args__ = (UniqueConstraint('uniprot_ac', 'uniprot_start', 'uniprot_stop', name='_unique_protein_region'),
+                     )
+    
     def __repr__(self):
         return "<Pfam(pfam_id='%s', name='%s', interpro_id='%s', uniprot_ac='%s', uniprot_start='%s', uniprot_stop='%s')>" % (
                             self.pfam_id, self.name, self.interpro_id, self.uniprot_ac, self.uniprot_start, self.uniprot_stop)
@@ -203,92 +208,13 @@ class Gene(Base):
         return "<Gene(strand='%s', gene_name='%s', gencode_transcription_id='%s', gencode_translation_name='%s', gencode_gene_id='%s', havana_gene_id='%s', havana_translation_id='%s')>" % (
                             self.strand, self.gene_name, self.gencode_transcription_id, self.gencode_translation_name, self.gencode_gene_id, self.havana_gene_id, self.havana_translation_id)
         
-
-_locus = [
-        {
-            "locus":{
-                "chromosome":"11",
-                "position":65837965,
-            },
-            "locus_information": [
-                    {
-                        "information":{
-                            "chromosome":"11",
-                            "chromosome_position":65837965,
-                            'cDNA_position':8,
-                            'uniprot_position' : 2,
-                            'strand': '+',
-                            'allele': 'A',
-                            'codon': 'GAA',
-                            'codon_allele_position': 1,
-                            'amino_acid_residue': 'E',
-                            'gene_name' : 'PACS1',
-                            'gencode_transcription_id' : 'ENST00000320580.4',
-                            'gencode_translation_name' : 'PACS1-001',
-                            'gencode_gene_id' : 'ENSG00000175115.7',
-                            'havana_gene_id' : 'OTTHUMG00000166889.3',
-                            'havana_translation_id' : 'OTTHUMT00000391690.2',
-                            'uniprot_ac' : 'Q6VY07',
-                            'uniprot_name' : 'PACS1_HUMAN',
-                            'pfam_domain_consensus_position' : None,
-                            'pfam_domain_name' : None,
-                            'pfam_domain_id' : None,
-                            'interpro_id' : None,
-                            'uniprot_domain_start_pos' : None,
-                            'uniprot_domain_end_pos' : None,
-                        },
-                        "meta_information":[],
-                    },
-                ],
-        },
-         {
-            "locus":{
-                "chromosome":"11",
-                "position":66000499,
-            },
-            "locus_information": [
-                    {
-                        "information":{
-                            "chromosome":"11",
-                            "chromosome_position":66000499,
-                            'cDNA_position':1800,
-                            'uniprot_position' : 599,
-                            'strand': '+',
-                            'allele': 'G',
-                            'codon': 'CAG',
-                            'codon_allele_position': 2,
-                            'amino_acid_residue': 'Q',
-                            'gene_name' : 'PACS1',
-                            'gencode_transcription_id' : 'ENST00000320580.4',
-                            'gencode_translation_name' : 'PACS1-001',
-                            'gencode_gene_id' : 'ENSG00000175115.7',
-                            'havana_gene_id' : 'OTTHUMG00000166889.3',
-                            'havana_translation_id' : 'OTTHUMT00000391690.2',
-                            'uniprot_ac' : 'Q6VY07',
-                            'uniprot_name' : 'PACS1_HUMAN',
-                            'pfam_domain_consensus_position' : 123,
-                            'pfam_domain_name' : 'PACS-1 cytosolic sorting protein',
-                            'pfam_domain_id' : 'PF10254',
-                            'interpro_id' : 'IPR019381',
-                            'uniprot_domain_start_pos' : 548,
-                            'uniprot_domain_end_pos' : 958,
-                        },
-                        "meta_information":[
-                            "PACS2"
-                        ]
-                    },
-                ],
-        },
-    ]
-
 if __name__ == '__main__':
+    ## Simulate the database
     from sqlalchemy import create_engine
     engine = create_engine('sqlite:///:memory:', echo=True)
     Base.metadata.create_all(engine)
     
-#     chrompos = Chromosome(chromosome='11', position=66000499)
-
-    from sqlalchemy import MetaData
+    ## Save the schema of the database
     from sqlalchemy_schemadisplay import create_schema_graph
     
     # create the pydot graph object by autoloading all tables via a bound metadata object
@@ -299,4 +225,93 @@ if __name__ == '__main__':
        concentrate=False # Don't try to join the relation lines together
     )
     graph.write_png('dbschema.png') # write out the file
+    
+    ## test with real data
+    test_mapping_1 = Mapping(allele='G',
+                             codon='CAG',
+                             codon_allele_position=2,
+                             amino_acid_residue='Q',
+                             cDNA_position=1800,
+                             uniprot_position=599,
+                             pfam_consensus_position=52,)
+    
+    test_chrompos_1 = Chromosome(chromosome='11', position=66000499)
+    test_chrompos_1.mappings.append(test_mapping_1)
+    
+    test_mapping_2 = Mapping(allele='A',
+                             codon='GAA',
+                             codon_allele_position=1,
+                             amino_acid_residue='E',
+                             cDNA_position=8,
+                             uniprot_position=2,)
+    test_chrompos_2 = Chromosome(chromosome='11', position=65837965)
+    test_chrompos_2.mappings.append(test_mapping_2)
+
+    test_pacs1_mappings = [test_mapping_1, test_mapping_2]
+    
+    test_gene_1 = Gene(strand=Gene.Strand.plus, gene_name='PACS1',
+                gencode_transcription_id='ENST00000320580.4', 
+                gencode_translation_name='PACS1-001', 
+                gencode_gene_id='ENSG00000175115.7',
+                havana_gene_id='OTTHUMG00000166889.3',
+                havana_translation_id='OTTHUMT00000391690.2',)
+    
+    test_protein_1 = Protein(uniprot_ac='Q6VY07', 
+                             uniprot_name='PACS1_HUMAN', 
+                             source=Protein.ProteinSource.swissprot,)
+    
+    for mapping in test_pacs1_mappings: 
+        test_gene_1.mappings.append(mapping)
+        test_protein_1.mappings.append(mapping)
+    
+    test_pfam_domain_1 = Pfam(pfam_id='PF10254', 
+                              name='PACS-1 cytosolic sorting protein', 
+                              interpro_id='IPR019381',
+                              uniprot_ac='Q6VY07',
+                              uniprot_start=548,
+                              uniprot_stop=958,)
+    test_protein_1.pfam_domains.append(test_pfam_domain_1)
+    test_pfam_domain_1.mappings.append(test_mapping_1)
+    
+    
+    
+    
+    test_mapping_pacs2_1 = Mapping(allele='C',
+                             codon='CAG',
+                             codon_allele_position=0,
+                             amino_acid_residue='Q',
+                             cDNA_position=1585,
+                             uniprot_position=528,
+                             pfam_consensus_position=52,)
+    test_chrompos_pacs2_1 = Chromosome(chromosome='14', position=105849210)
+    test_chrompos_pacs2_1.mappings.append(test_mapping_pacs2_1)
+    
+    
+    test_protein_pacs2_1 = Protein(uniprot_ac='Q86VP3-2', 
+                             uniprot_name='PACS2_HUMAN', 
+                             source=Protein.ProteinSource.swissprot,)
+    test_protein_pacs2_1.mappings.append(test_mapping_pacs2_1)
+    
+    test_gene_pacs2_1 = Gene(strand=Gene.Strand.plus, gene_name='PACS2',
+                gencode_transcription_id='ENST00000458164.2', 
+                gencode_translation_name='PACS2-001', 
+                gencode_gene_id='ENSG00000179364.9',
+                havana_gene_id='OTTHUMG00000170450.1',
+                havana_translation_id='OTTHUMT00000409210.1',)
+    test_gene_pacs2_1.mappings.append(test_mapping_pacs2_1)
+    
+    test_pfam_domain_pacs2_1 = Pfam(pfam_id='PF10254', 
+                              name='PACS-1 cytosolic sorting protein', 
+                              interpro_id='IPR019381',
+                              uniprot_ac='Q86VP3-2',
+                              uniprot_start=477,
+                              uniprot_stop=900,)
+    test_protein_pacs2_1.pfam_domains.append(test_pfam_domain_pacs2_1)
+    test_pfam_domain_pacs2_1.mappings.append(test_mapping_pacs2_1)
+    
+    test = 0
+#     chromosome_id = Column(Integer, ForeignKey('chromosomes.id'), nullable=False)
+#     gene_id = Column(Integer, ForeignKey('genes.id'), nullable=False)
+#     protein_id = Column(Integer, ForeignKey('proteins.id'))
+#     pfam_domain_id = Column(Integer, ForeignKey('pfam_domains.id'))
     
