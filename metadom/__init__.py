d59b2ef5f4812cc24260a2e39f8b604dc9e8c64c
laurensvdwiel <laurensvdwiel@gmail.com>
2017-08-11 10:09:06 +0200

    added implementations from the XSSP-API

--- metadom/__init__.py
+++ metadom/__init__.py
@@ -1,5 +1,11 @@
 import logging
 
+
+_VERSION = '0.0.1'
+
 # Create the top-level logger. This is required because Flask's built-in method
 # results in loggers with the incorrect level.
 _log = logging.getLogger(__name__)
+
+def get_version():
+    return _VERSION
--- metadom/presentation/web/routes.py
+++ metadom/presentation/web/routes.py
@@ -1,6 +1,11 @@
 import logging
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app as app, url_for, request
+
+
+from metadom import get_version
+from metadom.presentation.web.forms import MetaDomForm
+# from metadom.services.xssp import process_request
 
 _log = logging.getLogger(__name__)
 
@@ -10,6 +15,20 @@ bp = Blueprint('web', __name__, static_folder='static',
 
 @bp.route('/', methods=['GET'])
 def index():
+#     form = MetaDomForm()
+#     if form.validate_on_submit():
+#         celery_id = process_request(form.input_type.data, form.output_type.data,
+#                                     form.pdb_id.data, request.files,
+#                                     form.sequence.data)
+
+#         _log.info("Redirecting to output page")
+#         return redirect(url_for('dashboard.output',
+#                                 input_type=form.input_type.data,
+#                                 output_type=form.output_type.data,
+#                                 celery_id=celery_id))
+    
+    _log.info("Rendering index page")
+    
     return render_template('index.html')
 
 
