fcb9ee2aec642ac9032174b3c691a7e818be74d8
laurensvdwiel <laurensvdwiel@gmail.com>
2017-08-11 16:00:45 +0200

    started integrating of postgresql

--- metadom/domain/MappingRepository.py
+++ metadom/domain/MappingRepository.py
@@ -1,10 +1,41 @@
+import logging
+
+from metadom.infrastructure.database import Session
+from metadom.domain.models.mapping import Mapping
+from metadom.domain.models.chromosome import Chromosome
+from metadom.domain.models.gene import Gene
+from metadom.domain.models.protein import Protein
+from metadom.domain.models.pfam import Pfam
+
+from sqlalchemy import and_
+
+_log = logging.getLogger(__name__)
+
 class MappingRepository:
 
     @staticmethod
     def get_mappings(entry_id, position):
-
-        # This is a pfam query
-        # mappings = session.query(Mapping).join(Pfam).filter(and_(
-            # Mapping.pfam_consensus_position==position,
-            # Pfam.pfam_id==entry_id))
-        return {}
+        
+        session = Session()
+        
+        # TODO: remove when testing is done
+        _log.info("got entry:" +str(entry_id)+" and position: "+str(position))
+        
+        # Default mapping
+        mapping = {}
+        
+        # TODO: create handling of specific type of queries
+        
+        # This is a pfam with position query
+        for x in session.query(Mapping).join(Pfam).filter(and_(
+            Mapping.pfam_consensus_position==position,
+            Pfam.pfam_id==entry_id)):
+            for y in session.query(Chromosome).filter(Chromosome.id ==
+                                                       x.chromosome_id):
+                if 'chromosome_positions' in mapping:
+                    mapping['chromosome_positions'].append((y,x))
+                else:
+                    mapping['chromosome_positions'] = [(y,x)]
+         
+            
+        return mapping
--- metadom/infrastructure/database.py
+++ metadom/infrastructure/database.py
@@ -1,4 +1,23 @@
 from sqlalchemy.ext.declarative.api import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# TODO: connect to db container
+# Declare the base
 Base = declarative_base()
+
+# an Engine, which the Session will use for connection
+# resources
+# TODO: connect to db container @metadom_db_1
+metadom_engine = create_engine('postgresql://metadom_user:example@metadom_db_1/')
+# Base.metadata.create_all(metadom_engine)
+
+# create a configured "Session" class
+Session = sessionmaker(bind=metadom_engine)
+
+
+#####
+# TODO: test if database exists, otherwise create 
+# session = Session()
+Base.metadata.create_all(metadom_engine)
+
+# session.commit()
