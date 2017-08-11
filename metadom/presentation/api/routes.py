eaad1e3a1fbdf4ad29f04de3634547594202acc2
laurensvdwiel <laurensvdwiel@gmail.com>
2017-08-11 10:09:54 +0200

    added api calls to better represent the underlying data

--- metadom/presentation/api/routes.py
+++ metadom/presentation/api/routes.py
@@ -1,100 +1,56 @@
 import logging
 
 from flask import abort, Blueprint, jsonify, render_template
+from metadom.domain.model.pfam import Pfam
+from metadom.domain.model.chromosome import Chromosome
+from metadom.domain.model.mapping import Mapping
+
+from sqlalchemy import and_
 
 _log = logging.getLogger(__name__)
 
 bp = Blueprint('api', __name__, url_prefix='/api', static_folder='static',
                template_folder='templates')
 
-# TODO: Delete this after testing is done
-_locus = [
-        {
-            "locus":{
-                "chromosome": 11,
-                "position": 65837965,
-                },
-            "locus_information": [
-                {
-                    "information": {
-                        "chromosome": 11,
-                        "chromosome_position": 65837965,
-                        'cDNA_position': 8,
-                        'uniprot_position': 2,
-                        'strand': '+',
-                        'allele': 'A',
-                        'codon': 'GAA',
-                        'codon_allele_position': 1,
-                        'amino_acid_residue': 'E',
-                        'gene_name': 'PACS1',
-                        'gencode_transcription_id': 'ENST00000320580.4',
-                        'gencode_translation_name': 'PACS1-001',
-                        'gencode_gene_id': 'ENSG00000175115.7',
-                        'havana_gene_id': 'OTTHUMG00000166889.3',
-                        'havana_translation_id': 'OTTHUMT00000391690.2',
-                        'uniprot_ac': 'Q6VY07',
-                        'uniprot_name': 'PACS1_HUMAN',
-                        'pfam_domain_consensus_position' : None,
-                        'pfam_domain_name' : None,
-                        'pfam_domain_id' : None,
-                        'interpro_id' : None,
-                        'uniprot_domain_start_pos' : None,
-                        'uniprot_domain_end_pos' : None,
-                        },
-                    "meta_information":[],
-                    },
-                ],
-            },
-        {
-            "locus": {
-                "chromosome": 11,
-                "position": 66000499,
-                },
-            "locus_information": [
-                {
-                    "information": {
-                        "chromosome": 11,
-                        "chromosome_position": 66000499,
-                        'cDNA_position': 1800,
-                        'uniprot_position': 599,
-                        'strand': '+',
-                        'allele': 'G',
-                        'codon': 'CAG',
-                        'codon_allele_position': 2,
-                        'amino_acid_residue': 'Q',
-                        'gene_name': 'PACS1',
-                        'gencode_transcription_id': 'ENST00000320580.4',
-                        'gencode_translation_name': 'PACS1-001',
-                        'gencode_gene_id': 'ENSG00000175115.7',
-                        'havana_gene_id': 'OTTHUMG00000166889.3',
-                        'havana_translation_id': 'OTTHUMT00000391690.2',
-                        'uniprot_ac': 'Q6VY07',
-                        'uniprot_name': 'PACS1_HUMAN',
-                        'pfam_domain_consensus_position': 123,
-                        'pfam_domain_name': 'PACS-1 cytosolic sorting protein',
-                        'pfam_domain_id': 'PF10254',
-                        'interpro_id': 'IPR019381',
-                        'uniprot_domain_start_pos' : 548,
-                        'uniprot_domain_end_pos' : 958,
-                        },
-                    "meta_information":[
-                        "PACS2"
-                        ]
-                    },
-                ],
-            },
-        ]
+# GET /api/chromosome/:id
+@bp.route('/chromosome/<string:chromosome_id>', methods=['GET'])
+def get_mapping_via_chr_pos(chromosome_id):
+    pass
+#     if len(locus) == 0:
+#         abort(404)
+#     return jsonify({'locus': locus[0]})
+
+# GET /api/mapping/:id
+@bp.route('/mapping/<string:mapping_id>', methods=['GET'])
+def get_mapping_via_id(mapping_id):
+    pass
+
+# GET /api/gene/:id
+@bp.route('/gene/<string:gene_id>', methods=['GET'])
+def get_mapping_via_gene_id(gene_id):
+    pass
 
 
-@bp.route('/chr/<int:chr_>/<int:position>', methods=['GET'])
-def get_chr_pos(chr_, position):
-    locus = [x for x in _locus
-             if x['locus']['chromosome'] == chr_
-             and x['locus']['position'] == position]
-    if len(locus) == 0:
-        abort(404)
-    return jsonify({'locus': locus[0]})
+# GET /api/protein/:id
+@bp.route('/protein/<string:protein_id>', methods=['GET'])
+def get_mapping_via_protein_id(protein_id):
+    pass
 
+# GET /api/pfam/:id
+@bp.route('/pfam/<string:pfam_id>', methods=['GET'])
+def get_mapping_via_pfam_id(pfam_id):
+#     for x in session.query(Mapping).join(Pfam).filter(Pfam.pfam_id == pfam_id):
+#         for y in session.query(Chromosome).filter(Chromosome.id == x.chromosome_id):
+#             print(y,x)
+    pass
+
+# GET /api/pfam/:id/:position
+@bp.route('/pfam/<string:pfam_id>/<int:position>', methods=['GET'])
+def get_mapping_via_pfam_position(pfam_id, position):
+#     for x in session.query(Mapping).join(Pfam).filter(and_(Mapping.pfam_consensus_position==position, Pfam.pfam_id == pfam_id)):
+#         for y in session.query(Chromosome).filter(Chromosome.id == x.chromosome_id):
+#             print(y,x)
+    pass
 
 @bp.errorhandler(Exception)
 def exception_error_handler(error):  # pragma: no cover
