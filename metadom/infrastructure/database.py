ae3b83f29ac4482b879fe7547d726680753cac9e
laurensvdwiel <laurensvdwiel@gmail.com>
2017-08-15 12:31:15 +0200

    added methods to interact with database

--- metadom/infrastructure/database.py
+++ metadom/infrastructure/database.py
@@ -1,23 +1,37 @@
 from sqlalchemy.ext.declarative.api import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
+import logging
+
+# Create the top-level logger. This is required because Flask's built-in method
+# results in loggers with the incorrect level.
+_log = logging.getLogger(__name__)
 
 # Declare the base
 Base = declarative_base()
 
 # an Engine, which the Session will use for connection
 # resources
-# TODO: connect to db container @metadom_db_1
 metadom_engine = create_engine('postgresql://metadom_user:example@metadom_db_1/')
 # Base.metadata.create_all(metadom_engine)
 
 # create a configured "Session" class
 Session = sessionmaker(bind=metadom_engine)
-
-
+ 
+ 
 #####
 # TODO: test if database exists, otherwise create 
 # session = Session()
 Base.metadata.create_all(metadom_engine)
 
-# session.commit()
+def list_tables():
+    return Base.metadata.tables.keys()
+
+def test_connection():
+    try:
+        _session = Session()
+        metadom_engine.connect()
+        _session.commit()
+    except OperationalError as e:
+        _log.error(e)
