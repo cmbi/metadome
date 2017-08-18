2c455c495352c83ee07d81cc00ff082b29ea8378
laurensvdwiel <laurensvdwiel@gmail.com>
2017-08-18 15:00:06 +0200

    added services to handle requests to the database

---
+++ metadom/domain/services.py
@@ -0,0 +1,30 @@
+import time
+
+from metadom import _log
+from psycopg2 import OperationalError
+
+def list_tables(_db):
+    return _db.Model.metadata.tables.keys()
+  
+def test_connection(_db):
+    try:
+        _db.session.commit()
+        return True
+    except _db.OperationalError as e:
+        _log.error(e)
+    return False
+# 
+# def initialize_database(app, _db, retry_count=None):
+#     if retry_count is None:
+#         retry_count = 0
+#     
+#     if retry_count < app.config['DB_RETRY_CONNECTION_ATTEMPTS']:
+#         try:
+#             _db.create_all()
+#             _db.session.commit()
+#         except OperationalError as e:
+#             _log.warning(str(e)+"... for "+str(retry_count)+" attempts. Reconnecting...")
+#             time.sleep(app.config['DB_RETRY_CONNECTION_TIMEOUT'])
+#             initialize_database(retry_count+1)
+#     else:
+#         _log.error("Could not connect to database after "+str(retry_count)+" attempts.")
