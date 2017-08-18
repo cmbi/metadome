6662c77e8d00c15b9ae4232dab779aa245a26ad3
laurensvdwiel <laurensvdwiel@gmail.com>
2017-08-18 16:00:57 +0200

    connected MappingRepository to the database

--- metadom/domain/repositories.py
+++ metadom/domain/repositories.py
@@ -1,14 +1,14 @@
 import logging
 
-# from metadom.application import db
+from metadom.database import db
 from metadom.domain.models.mapping import Mapping
 from metadom.domain.models.chromosome import Chromosome
 from metadom.domain.models.gene import Gene
 from metadom.domain.models.protein import Protein
 from metadom.domain.models.pfam import Pfam
 
-from sqlalchemy import and_
-from metadom.infrastructure.database import test_connection, list_tables
+# from sqlalchemy import and_
+from metadom.domain.services import test_connection, list_tables
 
 _log = logging.getLogger(__name__)
 
@@ -16,28 +16,28 @@ class MappingRepository:
 
     @staticmethod
     def get_mappings(entry_id, position):
-        _log.info("connection: "+str(test_connection()))
-        
-        _log.info(list_tables())
-        
         # TODO: remove when testing is done
+        _log.info("connection: "+str(test_connection()))
+        _log.info(list_tables())
         _log.info("got entry:" +str(entry_id)+" and position: "+str(position))
          
         # Default mapping
-        mapping = {1:'# TODO: one day... a real mapping will be here'}
-#         
-#         # TODO: create handling of specific type of queries
-#         
-#         # This is a pfam with position query
-#         for x in session.query(Mapping).join(Pfam).filter(and_(
-#             Mapping.pfam_consensus_position==position,
-#             Pfam.pfam_id==entry_id)):
-#             for y in session.query(Chromosome).filter(Chromosome.id ==
-#                                                        x.chromosome_id):
-#                 if 'chromosome_positions' in mapping:
-#                     mapping['chromosome_positions'].append((y,x))
-#                 else:
-#                     mapping['chromosome_positions'] = [(y,x)]
-#          
-#             
+        mapping = {}
+         
+        # TODO: create handling of specific type of queries
+        # TODO: make use of services
+        
+        # This is a pfam with position query
+        for x in db.session.query(Mapping).join(Pfam).filter(db.and_(
+            Mapping.pfam_consensus_position==position,
+            Pfam.pfam_id==entry_id)):
+            for y in db.session.query(Chromosome).filter(Chromosome.id ==
+                                                       x.chromosome_id):
+                chr_key = str(y.chromosome)+":"+str(y.position)
+                if chr_key in mapping:
+                    _log.error('Dublicate chromosome entry')
+                else:
+                    mapping[chr_key] = str(x)
+          
+        _log.info('got mapping: '+str(mapping))
         return mapping
