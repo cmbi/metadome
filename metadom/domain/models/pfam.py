f798d386143b82e3c93f4748e5e963b48f31232a
laurensvdwiel <laurensvdwiel@gmail.com>
2017-08-30 14:58:58 +0200

    changed the purpose of this table to reflect a protein domain occurrence

--- metadom/domain/models/pfam.py
+++ metadom/domain/models/pfam.py
@@ -3,18 +3,20 @@ from metadom.database import db
 class Pfam(db.Model):
     """
     Table: pfam_domains
+    Contains the representation of Pfam domain occurrences
+    in uniprot sequences
     
     Fields
     id                        identifier
     pfam_id                   Pfam identifier code 'PF#####'
     name                      Name of the Pfam domain
-    interpro_id               Interpro identifier  
-    uniprot_ac                uniprot accession code
+    pfam_length               Length of the Pfam domain
+    interpro_id               Interpro identifier
     uniprot_start             0 <= uniprot_start < uniprot_stop
     uniprot_stop              uniprot_stop <= uniprot_end
+    protein_id                Foreign key
     
     Relationships
-    one to many               mappings
     many to one               protein
     """
     # Table configuration
@@ -24,19 +26,24 @@ class Pfam(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pfam_id = db.Column(db.String(12), nullable=False)
     name = db.Column(db.String)
+    pfam_length = db.Column(db.Integer, nullable=False)
     interpro_id = db.Column(db.String(12))
-    uniprot_ac = db.Column(db.String(12), db.ForeignKey('proteins.uniprot_ac'), nullable=False)
     uniprot_start = db.Column(db.Integer, nullable=False)
     uniprot_stop = db.Column(db.Integer, nullable=False)
+    protein_id = db.Column(db.Integer, db.ForeignKey('proteins.id'), nullable=False)
     
     # Relationships
     protein = db.relationship("Protein", back_populates="pfam_domains")
-    mappings = db.relationship('Mapping', back_populates="pfam_domain")
+    pfam_domain_alignments = db.relationship("PfamDomainAlignment", back_populates="pfam_domain")
     
     # Constraints
-    __table_args__ = (db.UniqueConstraint('uniprot_ac', 'uniprot_start', 'uniprot_stop', name='_unique_protein_region'),
+    __table_args__ = (db.UniqueConstraint('protein_id', 'pfam_id', 'uniprot_start', 'uniprot_stop', name='_unique_protein_region'),
                      )
     
+    def get_alignment(self):
+        # TODO: create this method
+        pass
+    
     def __repr__(self):
-        return "<Pfam(pfam_id='%s', name='%s', interpro_id='%s', uniprot_ac='%s', uniprot_start='%s', uniprot_stop='%s')>" % (
-                            self.pfam_id, self.name, self.interpro_id, self.uniprot_ac, self.uniprot_start, self.uniprot_stop)
+        return "<Pfam(pfam_id='%s', name='%s', pfam_length='%s', interpro_id='%s', uniprot_start='%s', uniprot_stop='%s')>" % (
+                            self.pfam_id, self.name, self.pfam_length, self.interpro_id, self.uniprot_start, self.uniprot_stop)
