27728429dc5b9f20deb78829e3c7bcb5205d6784
laurensvdwiel <laurensvdwiel@gmail.com>
2017-08-30 14:52:26 +0200

    added new fields and change relation pfam_domain to pfam_domain_alignment

--- metadom/domain/models/mapping.py
+++ metadom/domain/models/mapping.py
@@ -3,29 +3,34 @@ from metadom.database import db
 class Mapping(db.Model):
     """
     Table: mapping
+    Representation of a single positional mapping between a gencode gene 
+    translation and a uniprot protein sequence
     
     Fields
     id                        identifier
     allele                    'A', 'T', 'C', 'G'
     codon                     all triplet combinations of the alleles 
     codon_allele_position     0, 1, 2, or None
-    amino_acid_residue        one of the 20 amino acids, * or None
+    amino_acid_residue        one of the 20 amino acids in the gene translation,
+                              * or None
+    amino_acid_position       the position in the amino acid sequence of the 
+                              gene translation
     cDNA_position             the position in the cDNA
+    uniprot_residue           one of the 20 amino acids in the uniprot sequence,
+                              * or None
     uniprot_position          the position in the protein
-    pfam_consensus_position   position in the aligned Pfam domain or None
     chromosome_id             Foreign key
     gene_id                   Foreign key
     protein_id                Foreign key
-    pfam_domain_id            Foreign key
     
     Relationships
     many to one               chromosome
     many to one               gene
     many to one               protein
-    many to one               pfam_domain
+    one to many               pfam_domain_alignment
     """
     # Table configuration
-    __tablename__ = 'mapping'
+    __tablename__ = 'mappings'
     
     # Fields
     id = db.Column(db.Integer, primary_key=True)
@@ -34,22 +39,20 @@ class Mapping(db.Model):
     codon_allele_position = db.Column(db.Integer)
     amino_acid_residue = db.Column(db.String(1))
     amino_acid_position = db.Column(db.Integer)
-    uniprot_residue = db.Column(db.String(1))
     cDNA_position = db.Column(db.Integer)
+    uniprot_residue = db.Column(db.String(1))
     uniprot_position = db.Column(db.Integer)
-    pfam_consensus_position = db.Column(db.Integer)
     chromosome_id = db.Column(db.Integer, db.ForeignKey('chromosomes.id'), nullable=False)
     gene_id = db.Column(db.Integer, db.ForeignKey('genes.id'), nullable=False)
     protein_id = db.Column(db.Integer, db.ForeignKey('proteins.id'))
-    pfam_domain_id = db.Column(db.Integer, db.ForeignKey('pfam_domains.id'), nullable=True)
     
     # Relationships
     chromosome = db.relationship('Chromosome', back_populates="mappings")
     gene = db.relationship("Gene", back_populates="mappings")
     protein = db.relationship("Protein", back_populates="mappings")
-    pfam_domain = db.relationship("Pfam", back_populates="mappings")
+    pfam_domain_alignment = db.relationship("PfamDomainAlignment", back_populates="mappings")
     
     def __repr__(self):
-        return "<Mapping(allele='%s', codon='%s', codon_allele_position='%s', amino_acid_residue='%s', cDNA_position='%s', uniprot_position='%s', pfam_consensus_position='%s')>" % (
-                            self.allele, self.codon, self.codon_allele_position, self.amino_acid_residue, self.cDNA_position, self.uniprot_position, self.pfam_consensus_position)
+        return "<Mapping(allele='%s', codon='%s', codon_allele_position='%s', amino_acid_residue='%s', cDNA_position='%s', uniprot_position='%s')>" % (
+                            self.allele, self.codon, self.codon_allele_position, self.amino_acid_residue, self.cDNA_position, self.uniprot_position)
     
