9b9fb1799617a3da27f62ced4f5720df41d641d7
laurensvdwiel <laurensvdwiel@gmail.com>
2017-09-07 11:17:36 +0200

    rewritten the model for domain alignments

--- metadom/domain/models/pfam_domain_alignment.py
+++ metadom/domain/models/pfam_domain_alignment.py
@@ -3,13 +3,13 @@ from metadom.database import db
 class PfamDomainAlignment(db.Model):
     """
     Table: pfam_domain_alignment
-    Contains single alignment positions of uniprot residues to 
+    Contains a aligned positions of uniprot residues to 
     Pfam domain occurrences in uniprot sequences
     
     Fields
     id                        identifier
-    uniprot_position          position of aligned uniprot position
-    pfam_position             The consensus position of the pfam domain
+    domain_consensus_position The consensus position of the pfam domain
+    domain_consensus_residue  The consensus residue of the pfam domain
     mapping_id                Foreign key
     protein_id                Foreign key
     
@@ -22,19 +22,19 @@ class PfamDomainAlignment(db.Model):
     
     # Fields
     id = db.Column(db.Integer, primary_key=True)
-    uniprot_position = db.Column(db.Integer, nullable=False)
-    pfam_position = db.Column(db.Integer)
-    mapping_id = db.Column(db.Integer, db.ForeignKey('mappings.id'), nullable=True)
-    pfam_id = db.Column(db.Integer, db.ForeignKey('interpro_domains.id'), nullable=False)
+    domain_consensus_position = db.Column(db.Integer)
+    domain_consensus_residue = db.Column(db.String(1))
+    mapping_id = db.Column(db.Integer, db.ForeignKey('mappings.id'), nullable=False)
+    domain_id = db.Column(db.Integer, db.ForeignKey('interpro_domains.id'), nullable=False)
     
     # Relationships
     mappings = db.relationship('Mapping', back_populates="pfam_domain_alignment")
-    pfam_domain = db.relationship('Interpro', back_populates="pfam_domain_alignments")
+    domain = db.relationship('Interpro', back_populates="pfam_domain_alignments")
     
     # Constraints
-    __table_args__ = (db.UniqueConstraint('pfam_id', 'uniprot_position', name='_unique_protein_domain_position'),
+    __table_args__ = (db.UniqueConstraint('domain_id', 'mapping_id', name='_unique_protein_domain_position'),
                      )
-     
+    
     def __repr__(self):
-        return "<PfamDomainAlignment(pfam_id='%s', mapping_id='%s', uniprot_position='%s', pfam_position='%s')>" % (
-                            self.pfam_id, self.mapping_id, self.uniprot_position, self.pfam_position)
+        return "<PfamDomainAlignment(domain_id='%s', mapping_id='%s')>" % (
+                            self.domain_id, self.mapping_id)
