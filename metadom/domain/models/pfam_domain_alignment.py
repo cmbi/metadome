e46621eb8d0826dcf89ee27451350a8121271d08
laurensvdwiel <laurensvdwiel@gmail.com>
2017-09-26 17:51:50 +0200

    added ailgnment positions and getters so alignments may be parsed back to the user

--- metadom/domain/models/pfam_domain_alignment.py
+++ metadom/domain/models/pfam_domain_alignment.py
@@ -1,4 +1,6 @@
 from metadom.database import db
+from metadom.domain.models.mapping import Mapping
+from metadom.domain.models.protein import Protein
 
 class PfamDomainAlignment(db.Model):
     """
@@ -22,6 +24,7 @@ class PfamDomainAlignment(db.Model):
     
     # Fields
     id = db.Column(db.Integer, primary_key=True)
+    alignment_position = db.Column(db.Integer, nullable=False)
     domain_consensus_position = db.Column(db.Integer)
     domain_consensus_residue = db.Column(db.String(1))
     mapping_id = db.Column(db.Integer, db.ForeignKey('mappings.id'), nullable=False)
@@ -34,7 +37,25 @@ class PfamDomainAlignment(db.Model):
     # Constraints
     __table_args__ = (db.UniqueConstraint('domain_id', 'mapping_id', name='_unique_protein_domain_position'),
                      )
+        
+    def get_aligned_residue(self):
+        aligned_residue = Mapping.query.filter_by(id = self.mapping_id).first().uniprot_residue
+        
+        # double check if this is and insertion or an aligned residue
+        if self.domain_consensus_position is None and self.domain_consensus_residue == '-':
+            return aligned_residue.lower()
+        else:
+            return aligned_residue
+    
+    def get_protein(self):
+        return Protein.query.join(Mapping).filter((Mapping.id == self.mapping_id)).first()
+
+    def get_gene(self):
+        return Gene.query.join(Mapping).filter((Mapping.id == self.mapping_id)).first()
+      
+    def get_domain_occurence(self):
+        return Interpro.query.filter(id = domain_id).first()
     
     def __repr__(self):
-        return "<PfamDomainAlignment(domain_id='%s', mapping_id='%s')>" % (
-                            self.domain_id, self.mapping_id)
+        return "<PfamDomainAlignment(domain_id='%s', alignment_position='%s', mapping_id='%s')>" % (
+                            self.domain_id, self.alignment_position, self.mapping_id)
