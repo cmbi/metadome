a4e982dd2ade06f3ab272a80f70af5a5cdc4a416
laurensvdwiel <laurensvdwiel@gmail.com>
2017-12-08 11:27:52 +0100

    removed the db.session entirely from the mapping generation

--- metadom/domain/data_generation/mapping/Gene2ProteinMapping.py
+++ metadom/domain/data_generation/mapping/Gene2ProteinMapping.py
@@ -27,82 +27,81 @@ def convertListOfIntegerToRanges(p):
     yield (q[i], q[-1])
 
 def createMappingOfGeneTranscriptionToTranslationToProtein(gene_transcription, matching_coding_translation, uniprot):
-    with db.session.no_autoflush:
-        # Retrieve the amino acid sequence according to the translation
-        gene_protein_translation_sequence = matching_coding_translation['sequence']
-        # Retrieve the canonical uniprot sequence
-        canonical_protein_sequence = uniprot["sequence"]
-        # nucleotide sequence containing of the CDS
-        coding_sequence = gene_transcription['coding-sequence']
-        # CDS for nucleotide positions
-        cds =  gene_transcription['CDS_annotation']
+    # Retrieve the amino acid sequence according to the translation
+    gene_protein_translation_sequence = matching_coding_translation['sequence']
+    # Retrieve the canonical uniprot sequence
+    canonical_protein_sequence = uniprot["sequence"]
+    # nucleotide sequence containing of the CDS
+    coding_sequence = gene_transcription['coding-sequence']
+    # CDS for nucleotide positions
+    cds =  gene_transcription['CDS_annotation']
+        
+    # check if there are multiple chromosomal positions
+    chr_in_cds = list(set([cd.seqid for cd in cds]))
+    if len(chr_in_cds) > 1:
+        _log.warning('Found multiple chromosomes in coding sequence: '+str(chr_in_cds)+', using only '+str(chr_in_cds[0])+', this may indicate pseudoautosomal genes')
+    
+    # ensure we have the stop codon at the end of the translation sequence
+    gene_protein_translation_sequence+='*'
+    
+    # align cDNA sequence with uniprot canonical sequence
+    translation_to_uniprot_mapping  = createMappingOfAASequenceToAASequence(gene_protein_translation_sequence, canonical_protein_sequence)
+    
+    # Retrieve the codons from the cDNA translation 
+    translationCodons = []
+    for i in range(0, len(gene_protein_translation_sequence)): translationCodons.append(coding_sequence[i * 3 : i * 3 + 3])
+    
+    # Create mapping between Gene and cDNA
+    cDNA_pos = 0
+    currentChr = ''
+    to_be_added_chrom_pos = dict()
+    to_be_added_mapping = dict()
+    for cd in cds:
+        if currentChr == '': currentChr = cd.seqid # set it as the first
+        elif not(cd.seqid == currentChr):
+            _log.warning('Coding sequence with '+str(cd.seqid)+' does not match on chromosome '+str(currentChr)+', skipping this coding sequence...')
+            continue # skip this Chromosome
+        if cd.strand == '-':
+            custom_range = range(cd.end, cd.start-1, -1)
+        else:
+            custom_range = range(cd.start, cd.end+1)
+        for i in custom_range:
+            # create mapping object for this position
+            base_pair = coding_sequence[cDNA_pos]
             
-        # check if there are multiple chromosomal positions
-        chr_in_cds = list(set([cd.seqid for cd in cds]))
-        if len(chr_in_cds) > 1:
-            _log.warning('Found multiple chromosomes in coding sequence: '+str(chr_in_cds)+', using only '+str(chr_in_cds[0])+', this may indicate pseudoautosomal genes')
-        
-        # ensure we have the stop codon at the end of the translation sequence
-        gene_protein_translation_sequence+='*'
-        
-        # align cDNA sequence with uniprot canonical sequence
-        translation_to_uniprot_mapping  = createMappingOfAASequenceToAASequence(gene_protein_translation_sequence, canonical_protein_sequence)
-        
-        # Retrieve the codons from the cDNA translation 
-        translationCodons = []
-        for i in range(0, len(gene_protein_translation_sequence)): translationCodons.append(coding_sequence[i * 3 : i * 3 + 3])
-        
-        # Create mapping between Gene and cDNA
-        cDNA_pos = 0
-        currentChr = ''
-        to_be_added_chrom_pos = dict()
-        to_be_added_mapping = dict()
-        for cd in cds:
-            if currentChr == '': currentChr = cd.seqid # set it as the first
-            elif not(cd.seqid == currentChr):
-                _log.warning('Coding sequence with '+str(cd.seqid)+' does not match on chromosome '+str(currentChr)+', skipping this coding sequence...')
-                continue # skip this Chromosome
-            if cd.strand == '-':
-                custom_range = range(cd.end, cd.start-1, -1)
+            # increment cDNA position
+            cDNA_pos = cDNA_pos+1
+            aa_pos = int((cDNA_pos-1) / 3)
+            
+            # create chromosome entry
+            chrom_pos = Chromosome(chromosome=str(cd.seqid), position=i)
+            to_be_added_chrom_pos[chrom_pos.position] = chrom_pos
+            
+            # add codon to the mapping
+            codon = translationCodons[aa_pos]                
+            # add codon base pair number to the mapping
+            codon_base_pair_position = ((cDNA_pos-1)%3)
+            # Add residue from translation
+            amino_acid_residue = gene_protein_translation_sequence[aa_pos]
+            # Add information for uniprot
+            if amino_acid_residue == '*':
+                uniprot_residue = '*'
+                uniprot_position = None
+                aa_pos = None
             else:
-                custom_range = range(cd.start, cd.end+1)
-            for i in custom_range:
-                # create mapping object for this position
-                base_pair = coding_sequence[cDNA_pos]
-                
-                # increment cDNA position
-                cDNA_pos = cDNA_pos+1
-                aa_pos = int((cDNA_pos-1) / 3)
-                
-                # create chromosome entry
-                chrom_pos = Chromosome(chromosome=str(cd.seqid), position=i)
-                to_be_added_chrom_pos[chrom_pos.position] = chrom_pos
-                
-                # add codon to the mapping
-                codon = translationCodons[aa_pos]                
-                # add codon base pair number to the mapping
-                codon_base_pair_position = ((cDNA_pos-1)%3)
-                # Add residue from translation
-                amino_acid_residue = gene_protein_translation_sequence[aa_pos]
-                # Add information for uniprot
-                if amino_acid_residue == '*':
-                    uniprot_residue = '*'
-                    uniprot_position = None
-                    aa_pos = None
-                else:
-                    uniprot_position, uniprot_residue = map_single_residue(translation_to_uniprot_mapping, aa_pos)
-                
-                # create the mapping entry
-                to_be_added_mapping[chrom_pos.position] = Mapping(
-                    base_pair = base_pair,
-                    cDNA_position = cDNA_pos,
-                    codon = codon,
-                    codon_base_pair_position = codon_base_pair_position,
-                    amino_acid_residue = amino_acid_residue,
-                    amino_acid_position = aa_pos,
-                    uniprot_position = uniprot_position,
-                    uniprot_residue = uniprot_residue
-                    )
+                uniprot_position, uniprot_residue = map_single_residue(translation_to_uniprot_mapping, aa_pos)
+            
+            # create the mapping entry
+            to_be_added_mapping[chrom_pos.position] = Mapping(
+                base_pair = base_pair,
+                cDNA_position = cDNA_pos,
+                codon = codon,
+                codon_base_pair_position = codon_base_pair_position,
+                amino_acid_residue = amino_acid_residue,
+                amino_acid_position = aa_pos,
+                uniprot_position = uniprot_position,
+                uniprot_residue = uniprot_residue
+                )
                 
     return to_be_added_chrom_pos, to_be_added_mapping
 
