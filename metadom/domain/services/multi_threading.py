cf8f26f7905c947f21cc7f37c98c1661641f7af3
laurensvdwiel <laurensvdwiel@gmail.com>
2018-03-07 10:11:08 +0100

    revised the way services are constructed, now these are separated in various modules

---
+++ metadom/domain/services/multi_threading.py
@@ -0,0 +1,9 @@
+from sklearn.externals.joblib.parallel import cpu_count
+
+def CalculateNumberOfActiveThreads(numberOfTasks):
+    if(cpu_count() == 2):
+        return cpu_count()
+    elif numberOfTasks < cpu_count():
+        return numberOfTasks
+    else:
+        return cpu_count()
