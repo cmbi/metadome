e81b6f307fbce98b8c02e8fe1d81a9ab1ac57f73
laurensvdwiel <laurensvdwiel@gmail.com>
2018-03-15 13:05:53 +0100

    added dictionary that contains a per-protein id mapping from uniprot positions to consensus positions

--- metadom/domain/models/entities/meta_domain.py
+++ metadom/domain/models/entities/meta_domain.py
@@ -1,5 +1,5 @@
 from metadom.domain.repositories import InterproRepository
-from metadom.domain.data_generation.mapping.mapping_generator import generate_pfam_alignment_mappings
+from metadom.domain.data_generation.mapping.meta_domain_mapping import generate_pfam_alignment_mappings
 
 class UnsupportedMetaDomainIdentifier(Exception):
     pass
@@ -19,13 +19,15 @@ class MetaDomain(object):
     n_genes                    int number of unique genes containing this domain
     n_instances                int number of unique instances containing this domain
     mappings_per_consensus_pos dictionary of mappings per metadomain consensus positions; {POS: [models.mapping.Mapping]}
+    consensus_pos_per_protein  dictionary of protein ids with their uniprot positions mapped to consensus {protein_is: {uniprot_pos:<int:consensus_position>}}
     """
     domain_id = str() 
     consensus_length = int()
     n_genes = int()
     n_instances = int()
     mappings_per_consensus_pos = dict()
-
+    consensus_pos_per_protein = dict()
+    
     def __init__(self, domain_id):
         if domain_id.startswith('PF'):
             self.domain_id = domain_id
@@ -35,6 +37,6 @@ class MetaDomain(object):
             if len(domain_of_interest_occurrences) == 0:
                 raise NotEnoughOccurrencesForMetaDomain("There are not enough occurrences for the '"+str(self.domain_id)+"' to create a meta domain")
             
-            self.mappings_per_consensus_pos = generate_pfam_alignment_mappings(self.domain_id, domain_of_interest_occurrences)
+            self.mappings_per_consensus_pos, self.consensus_pos_per_protein = generate_pfam_alignment_mappings(self.domain_id, domain_of_interest_occurrences)
         else:
             raise UnsupportedMetaDomainIdentifier("Expected a Pfam domain, instead the identifier '"+str(domain_id)+"' was received")    
