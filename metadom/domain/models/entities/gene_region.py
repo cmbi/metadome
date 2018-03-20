61d8b20f2cfef0dddc5c2f907c802ffc01ff124e
laurensvdwiel <laurensvdwiel@gmail.com>
2018-03-20 17:01:43 +0100

    revised the way the GeneRegion object is constructed by adjusting fields into the __init__; added check to test if a domain_id is present in the region; added method to retreve the mappings per chromosome; changed the mappings to be set per cDNA instead of chromosome per default; added protein position to cDNA position mapping;

--- metadom/domain/models/entities/gene_region.py
+++ metadom/domain/models/entities/gene_region.py
@@ -1,5 +1,4 @@
 from metadom.domain.repositories import MappingRepository, ProteinRepository, InterproRepository
-import numpy as np
 
 class FailedToConstructGeneRegion(Exception):
     pass
@@ -32,23 +31,10 @@ class GeneRegion(object):
     regions                   list of regions
     mappings_per_chromosome   dictionary of mappings per chromosome positions; {POS: models.mapping.Mapping}
     """
-    chr = str()
-    gene_name = str()
-    gencode_transcription_id = str()
-    strand = str()
-    protein_region_start = int()
-    protein_region_stop = int()
-    protein_region_length = int()
-    cDNA_region_length = int()
-    protein_id = int()
-    uniprot_ac = str()
-    uniprot_name = str()
-    interpro_domains = []
-    regions = []
-    mappings_per_chromosome = dict()
     
-    # Source: http://stackoverflow.com/questions/4628333/converting-a-list-of-integers-into-range-in-python 
     def convertListOfIntegerToRanges(self, p):
+        """Converts a list of integer to a list of ranges.
+        Source: http://stackoverflow.com/questions/4628333/converting-a-list-of-integers-into-range-in-python"""
         if len(p) > 0:
             q = sorted(p)
             i = 0
@@ -57,28 +43,53 @@ class GeneRegion(object):
                     yield (q[i],q[j-1])
                     i = j
             yield (q[i], q[-1])
-            
+    
+    def domain_in_gene_region(self, domain_id):
+        """Checks if a given domain id is present in the region"""
+        for domain in self.interpro_domains:
+            if domain.ext_db_id == domain_id:
+                return True
+        return False
+    
+    def retrieve_mappings_per_chromosome(self):
+        """Returns the mappings for this gene region per chromosome position"""
+        mappings_per_chromosome = dict()
+        for chromosome_position in self.chromosome_pos_to_cDNA.keys():
+            mappings_per_chromosome[chromosome_position] = self.mappings_per_cDNA[self.chromosome_pos_to_cDNA[chromosome_position]]
+        return mappings_per_chromosome
+    
     def get_domains_for_position(self, position):
+        """Checks if there are any protein domains present at the given position"""
         return [domain for domain in self.interpro_domains if domain.uniprot_start-1 <= position <= domain.uniprot_stop]
-            
-    def __init__(self, _gene, _region_start=None, _region_stop=None):
-        if _region_start is None or _region_start < 0:
-            self.protein_region_start = 0
-            
-        if _region_stop is None or _region_stop > _gene.sequence_length:
-            self.protein_region_stop = _gene.sequence_length
-            
-        # perform additional type checking
-        if self.protein_region_stop < self.protein_region_start:
-            raise MalformedGeneRegionException("For transcript '"+str(self.gencode_transcription_id)+
-                                              "': attempted to build a gene region where_region_stop < _region_start")
-            
+    
+    
+    def __init__(self, _gene):
+        self.chr = str()
+        self.gene_name = str()
+        self.gencode_transcription_id = str()
+        self.strand = str()
+        self.protein_region_start = int()
+        self.protein_region_stop = int()
+        self.protein_region_length = int()
+        self.cDNA_region_length = int()
+        self.protein_id = int()
+        self.uniprot_ac = str()
+        self.uniprot_name = str()
+        self.interpro_domains = []
+        self.regions = []
+        self.mappings_per_cDNA = dict()
+        self.chromosome_pos_to_cDNA = dict()
+        self.protein_pos_to_cDNA = dict()
+        self.initialize_from_gene(_gene)
+    
+    def initialize_from_gene(self, _gene):
         # First set the fields that can directly be set from the _gene
         self.gene_name = _gene.gene_name
+        self.protein_region_start = 0
+        self.protein_region_stop = _gene.sequence_length            
         self.gencode_transcription_id = _gene.gencode_transcription_id
         self.protein_region_length = self.protein_region_stop - self.protein_region_start
         self.strand = _gene.strand
-        
         
         # retrieve the protein id
         _protein = ProteinRepository.retrieve_protein(_gene.protein_id)
@@ -90,25 +101,17 @@ class GeneRegion(object):
         self.interpro_domains = InterproRepository.get_domains_for_protein(self.protein_id)
         
         # Retrieve mappings from the database
-        _mappings = {x.cDNA_position:x for x in MappingRepository.get_mappings_for_gene(_gene)}
+        self.mappings_per_cDNA = {x.cDNA_position:x for x in MappingRepository.get_mappings_for_gene(_gene)}
 
         # Check f everything went fine so far
-        if len(_mappings) == 0:
+        if len(self.mappings_per_cDNA) == 0:
             raise FailedToConstructGeneRegion("For transcript '"+str(self.gencode_transcription_id)+
                                               "': No proper mapping was found in the database,"+
                                               " failed to initialize GeneRegion object")
         
-        # Now generate the regions
-        # create list that will be filled with all chromosomal positons of this region
-        _chromosome_positions_in_region = []
-        
-        # keep track on uniprot residue positions
-        _uniprot_positions = []
-        _cDNA_positions = []
-        
         # iterate over all cDNA positions
-        for cDNA_position in sorted( _mappings.keys(), key=lambda x: int(x) ):
-            _mapping = _mappings[cDNA_position]
+        for cDNA_position in sorted( self.mappings_per_cDNA.keys(), key=lambda x: int(x) ):
+            _mapping = self.mappings_per_cDNA[cDNA_position]
             
             # type check protein id
             if self.protein_id !=  _mapping.protein_id:
@@ -129,28 +132,24 @@ class GeneRegion(object):
             if _mapping.uniprot_residue == '-' or _mapping.uniprot_residue == '*':
                 continue
             
-            # test if this position falls within the region
-            if self.protein_region_start <= _mapping.uniprot_position < self.protein_region_stop:
-                # Add to the mapping to the mappings_per_chromosome
-                self.mappings_per_chromosome[_mapping.chromosome_position] = _mapping
-                
-                # Add the chromosomal position to the list
-                _chromosome_positions_in_region.append(_mapping.chromosome_position)
-                _uniprot_positions.append(_mapping.uniprot_position)
-                _cDNA_positions.append(_mapping.cDNA_position)
+            # Add the other mapping of other positions to this cDDA
+            self.chromosome_pos_to_cDNA[_mapping.chromosome_position] = int(cDNA_position)
+            if not _mapping.uniprot_position in self.protein_pos_to_cDNA.keys():
+                self.protein_pos_to_cDNA[_mapping.uniprot_position] = []
+            self.protein_pos_to_cDNA[_mapping.uniprot_position].append(int(_mapping.cDNA_position))
         
         # ensure that the region is fully covered
-        if(self.protein_region_length != len(np.unique(_uniprot_positions))):
+        if(self.protein_region_length != len(self.protein_pos_to_cDNA.keys())):
             raise RegioncDNALengthDoesNotEqualProteinLengthException("For transcript '"+str(self.gencode_transcription_id)+
                                                                      "': Attempted to recover chromosomal region, but the region was not"+
                                                                      " perfectly aligned, expected AA sequence length to be '"+str(self.protein_region_length)+"'"+
-                                                                     ", but was '"+str(len(np.unique(_uniprot_positions)))+"'")
+                                                                     ", but was '"+str(len(self.protein_pos_to_cDNA.keys()))+"'")
         
         # now add the cDNA region length
-        self.cDNA_region_length = len(_cDNA_positions)
+        self.cDNA_region_length = len(self.mappings_per_cDNA.keys())
 
         # convert the chromosome to ranges
-        self.regions = list(self.convertListOfIntegerToRanges(sorted(_chromosome_positions_in_region)))
+        self.regions = list(self.convertListOfIntegerToRanges(sorted(self.chromosome_pos_to_cDNA.keys())))
 
     def __repr__(self):
         return "<GeneRegion(chr='%s', gene_name='%s', gencode_transcription_id='%s', protein_region_length='%s', cDNA_region_length='%s', strand='%s', number of chromosomal regions='%s')>" % (
