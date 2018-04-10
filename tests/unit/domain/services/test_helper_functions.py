2ea1380b8f7a5e75cf1b1321fa4bcf8caa0ab131
laurensvdwiel <laurensvdwiel@gmail.com>
2018-04-10 10:16:31 +0200

    added tests for convertListOfIntegerToRanges; added more tests for sliding window

--- tests/unit/domain/services/test_helper_functions.py
+++ tests/unit/domain/services/test_helper_functions.py
@@ -1,9 +1,19 @@
 import unittest
-from metadom.domain.services.helper_functions import create_sliding_window
+from metadom.domain.services.helper_functions import create_sliding_window, convertListOfIntegerToRanges
 
 class TestHelperFunctions(unittest.TestCase):
 
+    def test_convertListOfIntegerToRanges(self):
+        self.assertEqual([x for x in convertListOfIntegerToRanges([0])], [(0, 0)])
+        self.assertEqual([x for x in convertListOfIntegerToRanges([0,1])], [(0, 1)])
+        self.assertEqual([x for x in convertListOfIntegerToRanges([0,2])], [(0, 0), (2, 2)])
+        self.assertEqual([x for x in convertListOfIntegerToRanges([0,1,2,4,5,6])], [(0, 2), (4, 6)])
+        
+
     def test_create_sliding_windowName(self):
+        self.assertEqual(create_sliding_window(-1, 0), [])
+        self.assertEqual(create_sliding_window(0, 0), [])
+        
         self.assertEqual(create_sliding_window(5, -1), 
                          [{'sw_coverage': 1.0, 'sw_range': range(0, 0)},
                           {'sw_coverage': 1.0, 'sw_range': range(1, 1)},
