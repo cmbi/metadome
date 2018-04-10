6c6e3a144c9d416963d230e827d1b68f2316b345
laurensvdwiel <laurensvdwiel@gmail.com>
2018-04-10 13:45:36 +0200

    changed percentage sliding window to number of residues; added prebuild method with quality for constructing the sliding window

--- metadom/domain/services/computation/gene_region_computations.py
+++ metadom/domain/services/computation/gene_region_computations.py
@@ -2,8 +2,9 @@ from metadom.domain.services.annotation.annotation import annotateSNVs
 from metadom.domain.services.annotation.gene_region_annotators import annotateTranscriptWithExacData    
 from metadom.domain.metrics.GeneticTolerance import background_corrected_mosy_score
 from metadom.domain.services.computation.codon_computations import retrieve_variant_type_counts
+from metadom.domain.services.helper_functions import create_sliding_window
 
-def compute_tolerance_landscape(gene_region, slidingWindow, min_frequency=0.0):
+def compute_tolerance_landscape(gene_region, sliding_window_size, min_frequency=0.0):
     # Annotate exac information
     full_exac_annotations = annotateSNVs(annotateTranscriptWithExacData, 
                                          mappings_per_chr_pos=gene_region.retrieve_mappings_per_chromosome(),
@@ -20,40 +21,34 @@ def compute_tolerance_landscape(gene_region, slidingWindow, min_frequency=0.0):
                 filtered_exac_annotations[chrom_pos].append(exac_variant)
         
     variant_type_counts = retrieve_variant_type_counts(gene_region.retrieve_mappings_per_chromosome(), filtered_exac_annotations)
-
-    # compute the sliding window size
-    sliding_window_size = float(gene_region.protein_region_length)*slidingWindow
+    
+    # precompute the sliding window
+    sliding_window = create_sliding_window(gene_region.protein_region_length, sliding_window_size)
 
     # Calculate the sliding window over the gene region
     region_sliding_window = []
-    for i in range(gene_region.protein_region_length):
-        if sliding_window_size == 0:
-            region_i_start=i
-            region_i_stop=i
-        else:
-            region_i_start = i-(sliding_window_size/2)
-            region_i_stop = i+(sliding_window_size/2)-1
+    for i in range(len(sliding_window)):
+        # correct for gene region start
+        protein_pos = i + gene_region.protein_region_start
         
-        if region_i_start <= gene_region.protein_region_start or region_i_stop >= gene_region.protein_region_stop:
-            region_sliding_window.append({'pos':i, 'score':0})
-            continue
-        else:
-            total_missense_region_i = 0
-            total_missense_background_region_i = 0
-            total_synonymous_region_i = 0
-            total_synonymous_background_region_i = 0
-            
-            for j in range(int(region_i_start), int(region_i_stop)):
-                total_missense_region_i += variant_type_counts[j]['missense']
-                total_missense_background_region_i += variant_type_counts[j]['background_missense']
-                total_synonymous_region_i += variant_type_counts[j]['synonymous']
-                total_synonymous_background_region_i += variant_type_counts[j]['background_synonymous']
+        total_missense_region_i = 0
+        total_missense_background_region_i = 0
+        total_synonymous_region_i = 0
+        total_synonymous_background_region_i = 0
+        for j in sliding_window[i]['sw_range']:
+            # correct gene region for region start
+            j += gene_region.protein_region_start
+                        
+            total_missense_region_i += variant_type_counts[j]['missense']
+            total_missense_background_region_i += variant_type_counts[j]['background_missense']
+            total_synonymous_region_i += variant_type_counts[j]['synonymous']
+            total_synonymous_background_region_i += variant_type_counts[j]['background_synonymous']
 
-            region_i_dn_ds = background_corrected_mosy_score(missense=total_missense_region_i, 
-                                                             missense_background=total_missense_background_region_i,
-                                                             synonymous=total_synonymous_region_i,
-                                                             synonymous_background=total_synonymous_background_region_i)
+        region_i_dn_ds = background_corrected_mosy_score(missense=total_missense_region_i, 
+                                                         missense_background=total_missense_background_region_i,
+                                                         synonymous=total_synonymous_region_i,
+                                                         synonymous_background=total_synonymous_background_region_i)
 
-            region_sliding_window.append({'pos':i, 'score':region_i_dn_ds})
+        region_sliding_window.append({'pos':protein_pos, 'score':region_i_dn_ds, 'sw_coverage':sliding_window[i]['sw_coverage']})
 
     return region_sliding_window
