ab6ce7d04950454e281a0bfab4ba65e99e65dd92
laurensvdwiel <laurensvdwiel@gmail.com>
2018-04-20 16:34:43 +0200

    added contact form

--- metadom/presentation/web/forms.py
+++ metadom/presentation/web/forms.py
@@ -1,5 +1,9 @@
 from flask_wtf import Form
+from flask_wtf.form import FlaskForm
 from wtforms.fields import TextField
+from wtforms.fields.html5 import EmailField
+from wtforms.fields.simple import TextAreaField
+from wtforms import validators
 
 
 class MetaDomForm(Form):
@@ -8,3 +12,9 @@ class MetaDomForm(Form):
 
     def __init__(self, allowed_extensions=None, **kwargs):
         super(MetaDomForm, self).__init__(**kwargs)
+
+
+class SupportForm(FlaskForm):
+    email = EmailField('Email',
+                       [validators.DataRequired(), validators.Email()])
+    body = TextAreaField('Body')
