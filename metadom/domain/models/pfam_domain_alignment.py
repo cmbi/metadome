9c9de7ac71c6e627fb686b8995958f736780ea94
laurensvdwiel <laurensvdwiel@gmail.com>
2018-04-26 10:23:52 +0200

    commit for future archive puproses, meta-domains will be implemented in a different manner

--- metadom/domain/models/pfam_domain_alignment.py
+++ metadom/domain/models/pfam_domain_alignment.py
@@ -1,6 +1,4 @@
 from metadom.database import db
-from metadom.domain.models.mapping import Mapping
-from metadom.domain.models.protein import Protein
 
 class PfamDomainAlignment(db.Model):
     """
@@ -10,6 +8,8 @@ class PfamDomainAlignment(db.Model):
     
     Fields
     id                        identifier
+    ext_db_id                 External domain database identifier code
+    alignment_position        The position (or column) in the MSA
     domain_consensus_position The consensus position of the pfam domain
     domain_consensus_residue  The consensus residue of the pfam domain
     mapping_id                Foreign key
@@ -17,13 +17,14 @@ class PfamDomainAlignment(db.Model):
     
     Relationships
     many to one               mappings
-    many to one               protein
+    many to one               domain
     """
     # Table configuration
     __tablename__ = 'pfam_domain_alignments'
     
     # Fields
     id = db.Column(db.Integer, primary_key=True)
+    ext_db_id = db.Column(db.String, nullable=False)
     alignment_position = db.Column(db.Integer, nullable=False)
     domain_consensus_position = db.Column(db.Integer)
     domain_consensus_residue = db.Column(db.String(1))
@@ -33,28 +34,6 @@ class PfamDomainAlignment(db.Model):
     # Relationships
     mappings = db.relationship('Mapping', back_populates="pfam_domain_alignment")
     domain = db.relationship('Interpro', back_populates="pfam_domain_alignments")
-    
-    # Constraints
-    __table_args__ = (db.UniqueConstraint('domain_id', 'mapping_id', name='_unique_protein_domain_position'),
-                     )
-        
-    def get_aligned_residue(self):
-        aligned_residue = Mapping.query.filter_by(id = self.mapping_id).first().uniprot_residue
-        
-        # double check if this is and insertion or an aligned residue
-        if self.domain_consensus_position is None and self.domain_consensus_residue == '-':
-            return aligned_residue.lower()
-        else:
-            return aligned_residue
-    
-    def get_protein(self):
-        return Protein.query.join(Mapping).filter((Mapping.id == self.mapping_id)).first()
-
-    def get_gene(self):
-        return Gene.query.join(Mapping).filter((Mapping.id == self.mapping_id)).first()
-      
-    def get_domain_occurence(self):
-        return Interpro.query.filter(id = domain_id).first()
     
     def __repr__(self):
         return "<PfamDomainAlignment(domain_id='%s', alignment_position='%s', mapping_id='%s')>" % (
