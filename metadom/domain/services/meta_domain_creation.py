b26be4ad36929cd7c8cba91c10d5b4a5c7071903
laurensvdwiel <laurensvdwiel@gmail.com>
2018-05-02 14:07:37 +0200

    added the service to create all meta domains

---
+++ metadom/domain/services/meta_domain_creation.py
@@ -0,0 +1,36 @@
+from metadom.domain.repositories import InterproRepository
+from metadom.domain.data_generation.mapping.meta_domain_mapping import generate_pfam_alignments
+
+import logging
+import os
+from metadom.default_settings import METADOMAIN_DIR
+from metadom.domain.wrappers.hmmer import interpret_hmm_alignment_file
+
+_log = logging.getLogger(__name__)
+
+def create_metadomains(reconstruct):
+    _log.info(interpret_hmm_alignment_file(METADOMAIN_DIR+'PF00002/metadomain_alignments'))
+    # initialize custom logging framework
+    _log.info("Starting creation of all meta-domain alignments")
+ 
+    # the genes that are to be checked
+    domains_of_interest = [x for x in InterproRepository.get_all_Pfam_identifiers_suitable_for_metadomains()]
+     
+    # check for which domains there have already been made alignments
+    finished_meta_domains = os.listdir(METADOMAIN_DIR)
+     
+    # (re-) construct the meta domain alignments    
+    domains_processed = 1
+    domains_already_processed = 0
+ 
+    for domain_id in domains_of_interest:
+        if domain_id in finished_meta_domains and not reconstruct:
+            domains_already_processed+=1
+            continue
+         
+        _log.info("Starting creation of alignment for '"+str(domains_processed)+"' out of '"+str(len(domains_of_interest))+"'")
+        generate_pfam_alignments(domain_id)
+        _log.info("Finished creation of alignment for '"+str(domains_processed)+"' out of '"+str(len(domains_of_interest))+"'")
+        domains_processed+=1
+         
+    _log.info("Finished the creation of meta-domain alignments of '"+str(len(domains_of_interest))+"' domains, resulting in '"+str(domains_processed)+"' successful meta-domain alignments, previously processed (='"+str(domains_already_processed)+"')")
