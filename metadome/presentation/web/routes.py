225bef46c5017d9a9094f98dda12bfb89bb859c2
laurensvdwiel <laurensvdwiel@gmail.com>
2018-06-22 11:30:07 +0200

    added error message if mail server is not configure

--- metadome/presentation/web/routes.py
+++ metadome/presentation/web/routes.py
@@ -7,7 +7,7 @@ from metadome.domain.repositories import GeneRepository
 from metadome.domain.services.mail.mail import mail
 from metadome.presentation.web.forms import SupportForm
 from metadome.presentation import api
-from metadome.default_settings import DEFAULT_RECIPIENT
+from metadome.default_settings import DEFAULT_RECIPIENT, MAIL_SERVER
 
 import logging
 
@@ -31,6 +31,9 @@ def dashboard_js():
 
 @bp.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if DEFAULT_RECIPIENT is None or MAIL_SERVER is None:
+        return render_template('error.html', msg="Mail server is not configured, therefore there can be no mails sent to support."), 500
+    
     form = SupportForm()
     if form.validate_on_submit():
         email = form.email.data
