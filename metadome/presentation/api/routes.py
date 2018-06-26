5e5efac58c020ea1ab20c2cd3be8ccc57ec23559
laurensvdwiel <laurensvdwiel@gmail.com>
2018-06-26 16:29:19 +0200

    removed no longer used methods

--- metadome/presentation/api/routes.py
+++ metadome/presentation/api/routes.py
@@ -5,11 +5,10 @@ from metadome.domain.services.annotation.gene_region_annotators import annotateT
     annotateTranscriptWithGnomADData
 from metadome.domain.models.entities.meta_domain import MetaDomain
 from metadome.domain.repositories import GeneRepository
-from flask import abort, Blueprint, jsonify, render_template, session
+from flask import Blueprint, jsonify, render_template
 from flask.globals import request
 from builtins import Exception
 import traceback
-import jsonpickle
 
 import logging
 
@@ -20,10 +19,6 @@ bp = Blueprint('api', __name__)
 ##########################################
 #########    Route end points    #########
 ##########################################
-
-@bp.route('/', methods=['GET'])
-def api_doc():
-    return render_template('api/docs.html')
 
 @bp.route('/gene/geneToTranscript', methods=['GET'])
 def get_default_transcript_ids():
@@ -52,7 +47,7 @@ def get_tolerance_landscape():
     pass
 
 @bp.route('/gene/getToleranceLandscape/<transcript_id>/', methods=['GET'])
-def get_tolerance_landscape_for_transcript(transcript_id):    
+def get_tolerance_landscape_for_transcript(transcript_id):
     sliding_window = request.args.get('slidingwindow')
     if sliding_window is None:
         sliding_window = 10
@@ -156,65 +151,6 @@ def get_tolerance_landscape_for_transcript(transcript_id):
     else:
         return jsonify({'error': 'No gene region could be build for transcript '+str(transcript_id)}), 500
 
-@bp.route('/pfam/getMetaDomain/<string:domain_id>', methods=['GET'])
-def get_metadomain_for_pfam_id(domain_id):
-    metadomain = MetaDomain(domain_id)
-    return jsonify(str(metadomain))
-
-@bp.route('/gene/getMetaDomain/<string:transcript_id>/<string:domain_id>', methods=['GET'])
-def get_metadomains_for_transcript(transcript_id, domain_id, _jsonify=True):
-    # create the return value
-    return_value = dict()
-    
-    # Retrieve the gene from the database
-    gene = GeneRepository.retrieve_gene(transcript_id)
-    
-    if not gene is None:
-        # build the gene region
-        gene_region = GeneRegion(gene)
-    
-    if gene_region is None:
-        return jsonify({'error': 'Transcript '+str(transcript_id)+' not present in database'}), 500
-     
-    # Check if the domain is present in the gene region
-    domains_in_gene = gene_region.retrieve_specific_domains_in_gene(domain_id)
-    if len(domains_in_gene) == 0:
-        return jsonify({'error': 'Domain id '+str(domain_id)+' not present in transcript '+str(transcript_id)+' in the database'}), 500
-     
-    # create a metadomain for this domain
-    metadomain = MetaDomain(domain_id)
-         
-    # retrieve the context for this protein
-    protein_to_consensus_positions = metadomain.consensus_pos_per_protein[gene_region.uniprot_ac]
-    
-    meta_domain_data = []
-    # generate the return value
-    for protein_pos in protein_to_consensus_positions:
-        metadom_entry = create_meta_domain_entry(gene_region, metadomain, protein_to_consensus_positions, protein_pos)
-        
-        # add the metadome entry to the return value
-        meta_domain_data.append(metadom_entry)
-    
-    # Add general info on the gene, domain and protein
-    meta_domain_info = dict()
-    meta_domain_info['gene_name'] = gene_region.gene_name
-    meta_domain_info['protein_ac'] = gene_region.uniprot_ac
-    meta_domain_info['protein_name'] = gene_region.uniprot_name
-    meta_domain_info['transcript'] = gene_region.gencode_transcription_id
-    meta_domain_info['strand'] = gene_region.strand.value
-    meta_domain_info['protein_length'] = gene_region.protein_region_length
-    meta_domain_info['cDNA_length'] = gene_region.cDNA_region_length
-    meta_domain_info['domain_id'] = metadomain.domain_id
-    meta_domain_info['domain_occurrences'] = [{'start':domain.uniprot_start, 'stop':domain.uniprot_stop} for domain in domains_in_gene]
-    
-    ## add everything to the return value
-    # sort the return value
-    return_value['meta_domain_data'] = sorted(meta_domain_data, key=lambda k: k['protein_pos'])
-    return_value['meta_domain_info'] = meta_domain_info
-    
-    # return to caller
-    return conditional_jsonify(return_value, _jsonify)
-
 def create_meta_domain_entry(gene_region, metadomain, protein_to_consensus_positions, protein_pos):
     metadom_entry = {}
     metadom_entry['consensus_pos'] = protein_to_consensus_positions[protein_pos]
@@ -316,75 +252,11 @@ def create_meta_domain_entry(gene_region, metadomain, protein_to_consensus_posit
             
     return metadom_entry
 
-@bp.route('/gene/getMetaDomainInformation/<string:transcript_id>/<int:amino_acid_position>', methods=['GET'])
-def get_metadomain_information_for_gene_position(transcript_id, amino_acid_position):
-    # Retrieve the gene from the database
-    gene = GeneRepository.retrieve_gene(transcript_id)
-    
-    if not gene is None:
-        # build the gene region
-        gene_region = GeneRegion(gene)
-    
-    return_value = {}
-    if not gene_region is None:
-        interpro_domains = gene_region.get_domains_for_position(amino_acid_position)
-        
-        meta_domain_mappings = {}
-        
-        for interpro_domain in interpro_domains:
-            domain_id = interpro_domain.ext_db_id
-            metadomain = MetaDomain(domain_id)
-            
-            protein_to_consensus_positions = metadomain.consensus_pos_per_protein[gene_region.uniprot_ac]
-            
-            if amino_acid_position in protein_to_consensus_positions:
-                meta_domain_mappings[domain_id] = metadomain.mappings_per_consensus_pos[protein_to_consensus_positions[amino_acid_position]]
-                #TODO finalize
-
-        
-        return jsonify(str(meta_domain_mappings))
-        
-#         protein_to_consensus_positions = metadomain.consensus_pos_per_protein[gene.protein_id]
-#         
-#         for pos in protein_to_consensus_positions:
-#             return_value[pos] = str(metadomain.mappings_per_consensus_pos[protein_to_consensus_positions[pos]])
-#     
-    return jsonify(return_value)
-
 @bp.errorhandler(Exception)
 def exception_error_handler(error):  # pragma: no cover
     _log.error("Unhandled exception:\n{}".format(error))
     _log.error(traceback.print_exc())
     return render_template('error.html', msg=error, stack_trace=traceback.print_exc()), 500
-
-##########################################
-#########   Get/set functions    #########
-##########################################
-
-# def get_or_set_current_gene_region(transcript_id, force_rebuild=False):
-#     if not force_rebuild and 'gene_region' in session and not session['gene_region'] is None:
-#         gene_region = jsonpickle.decode(session['gene_region'])
-#         if not gene_region is None and gene_region.gencode_transcription_id == transcript_id:
-#             return gene_region
-#      
-#     # new transcript, lets clear the session
-#     session.clear()
-#      
-#     # Retrieve the gene from the database
-#     gene = GeneRepository.retrieve_gene(transcript_id)
-#      
-#     if not gene is None:
-#         try:
-#             # create the gene_region
-#             gene_region = GeneRegion(gene)            
-#             session['gene_region'] = jsonpickle.encode(gene_region)
-#             return jsonpickle.decode(session['gene_region'])
-#         except Exception as e:
-#             _log.error(e)
-#             _log.error(traceback.print_exc())
-#             # to ensure no bad data got into the session lets clear it
-#             session.clear()
-#     return None
 
 def conditional_jsonify(_value, _jsonify=True):
     """Conditianally jsonifies a given value"""
