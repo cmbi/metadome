d7e8522c9cadac49cc119ef62a352216033e2ee7
laurensvdwiel <laurensvdwiel@gmail.com>
2018-07-03 13:04:02 +0200

    changed the credentials back to default

--- metadome/flask_app_credentials.py
+++ metadome/flask_app_credentials.py
@@ -1,3 +1,3 @@
 # this is the secret key needed for user sessioning,
 # this must be changed into something else when running on production
-SECRET_KEY_CRED = 'aau67edfh43467579okmnhgwe4567890oi;kuhgewr3qrgdhfg'
+SECRET_KEY_CRED = '1234567890'
--- metadome/postgres_credentials.py
+++ metadome/postgres_credentials.py
@@ -2,4 +2,4 @@
 POSTGRES_USER='metadom_user'
 # The password needed to connect to the postgreSQL database for MetaDome
 # For security reasone, please change this to something else
-POSTGRES_PASSWORD='example'
+POSTGRES_PASSWORD='metadome_user_pw'
