28ecb8340ecf59b28f9585f17ccfe9541d5ee6f3
Coos Baakman <c.baakman@radboudumc.nl>
2018-07-13 11:08:57 +0200

    add traceback for exceptions

--- prebuild_all.py
+++ prebuild_all.py
@@ -1,12 +1,17 @@
+import traceback
+
 from metadome.application import app, celery
 from metadome.tasks import create_prebuild_visualization
 from metadome.domain.repositories import GeneRepository
 
 with app.app_context():
-	transcripts = GeneRepository.retrieve_all_transcript_ids_with_mappings()
+    transcripts = GeneRepository.retrieve_all_transcript_ids_with_mappings()
 
-	results = [create_prebuild_visualization.delay(transcript.gencode_transcription_id)
-		   for transcript in transcripts]
+    results = [create_prebuild_visualization.delay(transcript.gencode_transcription_id)
+               for transcript in transcripts]
 
-	for result in results:
-	    print(result.get())
+    for result in results:
+        try:
+            print(result.get())
+        except:
+            traceback.print_exc()
