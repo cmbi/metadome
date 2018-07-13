ecc2b488bfc3183e5fdcada203a937412013b63c
Coos Baakman <c.baakman@radboudumc.nl>
2018-07-13 10:27:58 +0200

    add prebuild script

---
+++ prebuild_all.py
@@ -0,0 +1,12 @@
+from metadome.application import app, celery
+from metadome.tasks import create_prebuild_visualization
+from metadome.domain.repositories import GeneRepository
+
+with app.app_context():
+	transcripts = GeneRepository.retrieve_all_transcript_ids_with_mappings()
+
+	results = [create_prebuild_visualization.delay(transcript.gencode_transcription_id)
+		   for transcript in transcripts]
+
+	for result in results:
+	    print(result.get())
