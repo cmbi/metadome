c78008b42bece3907a1b0b6f665f7baa41831e93
laurensvdwiel <laurensvdwiel@gmail.com>
2018-08-22 16:43:36 +0200

    revised meta domains to abide the new changes

--- metadome/domain/models/entities/meta_domain.py
+++ metadome/domain/models/entities/meta_domain.py
@@ -25,7 +25,7 @@ class MetaDomain(object):
     mappings_per_consensus_pos dictionary of mappings per metadomain consensus positions; {POS: [models.mapping.Mapping]}
     consensus_pos_per_protein  dictionary of protein ids with their uniprot positions mapped to consensus {protein_id: {uniprot_pos:<int:consensus_position>}}
     """
-    
+        
     def get_codons_aligned_to_consensus_position(self, consensus_position):
         """Retrieves meta_codons for this consensus position"""
         codons = []
@@ -49,7 +49,9 @@ class MetaDomain(object):
             for gene_id in mapping_per_gene_per_amino_acid_pos.keys():
                 for amino_acid_position in mapping_per_gene_per_amino_acid_pos[gene_id].keys():
                     try:
-                        codon = Codon(mapping_per_gene_per_amino_acid_pos[gene_id][amino_acid_position])
+                        _gencode_transcription_id = self.gencode_transcription_id_per_gene_id[gene_id]
+                        _uniprot_ac = self.protein_ac_per_protein_id[mapping_per_gene_per_amino_acid_pos[gene_id][amino_acid_position][0].protein_id]
+                        codon = Codon.initializeFromMapping(mapping_per_gene_per_amino_acid_pos[gene_id][amino_acid_position], _gencode_transcription_id, _uniprot_ac)
                         # aggregate duplicate chromosomal regions
                         if not codon.unique_str_representation() in unique_codons.keys():
                             unique_codons[codon.unique_str_representation()] = []
@@ -72,26 +74,25 @@ class MetaDomain(object):
                 
         # return the codons that correspond to this position
         return codons
-            
-            
+
     
     def __init__(self, domain_id):
-        self.domain_id = str() 
+        self.domain_id = domain_id
         self.consensus_length = int()
         self.n_proteins = int()
         self.n_instances = int()
         self.mappings_per_consensus_pos = dict()
         self.consensus_pos_per_protein = dict()
 
+        # Double check this conserns a Pfam domain
         if domain_id.startswith('PF'):
-            self.domain_id = domain_id
-            
             # create the meta domain mapping
-            self.mappings_per_consensus_pos, self.consensus_pos_per_protein, self.n_instances = generate_pfam_alignment_mappings(self.domain_id)
+            self.mappings_per_consensus_pos, self.consensus_pos_per_protein, self.n_instances, self.gencode_transcription_id_per_gene_id, self.protein_ac_per_protein_id = generate_pfam_alignment_mappings(self.domain_id)
             
             # set the remaining values
             self.consensus_length = len(self.mappings_per_consensus_pos)
             self.n_proteins = len(self.consensus_pos_per_protein)
+                
         else:
             raise UnsupportedMetaDomainIdentifier("Expected a Pfam domain, instead the identifier '"+str(domain_id)+"' was received")
     
