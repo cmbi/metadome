f912ad88d13d6705bfc45717c396caa51ea06bc2
laurensvdwiel <laurensvdwiel@gmail.com>
2018-08-22 10:37:18 +0200

    revised codons to now be constructed from mappings, but not needing any database variables after initialization; revised the method retrieve_mappings_per_chromosome now to return per poition basepairs instead of mapping objects

--- metadome/domain/models/entities/codon.py
+++ metadome/domain/models/entities/codon.py
@@ -12,18 +12,19 @@ class Codon(object):
     Used for a codon-representation of models.mapping.Mappings that make up a codon
     
     Variables
-    name                       description
-    mappings                   list of the mapping objects for these locations
-    mapping_ids                list the list of ids for the mappings associated to this codon
-    gencode_transcription_id   str the transcription id associated to this codon
-    uniprot_ac                 str the uniprot protein ac associated to this codon
-    strand                     Enum the strand represented as models.gene.Strand
-    base_pair_representation   str the base pair representation of this codon (e.g. ATG)
-    amino_acid_residue         str the amino acid residue of this codon
-    amino_acid_position        int the amino acid position of this codon in the protein or gene translation
-    chr                        str the chromosome
-    regions                    tuple the range of chromosomal positions of this codon
-    cDNA_position_range        tuple the range of cDNA positions of this codon
+    name                                   description
+    gencode_transcription_id               str the transcription id associated to this codon
+    uniprot_ac                             str the uniprot protein ac associated to this codon
+    strand                                 Enum the strand represented as models.gene.Strand
+    base_pair_representation               str the base pair representation of this codon (e.g. ATG)
+    amino_acid_residue                     str the amino acid residue of this codon
+    amino_acid_position                    int the amino acid position of this codon in the protein or gene translation
+    chr                                    str the chromosome
+    regions                                tuple the range of chromosomal positions of this codon
+    cDNA_position_range                    tuple the range of cDNA positions of this codon
+    chromosome_position_base_pair_one      int the position corresponding to the first base pair
+    chromosome_position_base_pair_two      int the position corresponding to the second base pair
+    chromosome_position_base_pair_three    int the position corresponding to the third base pair
     """
 
     def unique_str_representation(self):
@@ -32,8 +33,11 @@ class Codon(object):
     def retrieve_mappings_per_chromosome(self):
         """Returns the mappings for this codon per chromosome position"""
         mappings_per_chromosome = dict()
-        for mapping in self.mappings:
-            mappings_per_chromosome[mapping.chromosome_position] = mapping
+        
+        mappings_per_chromosome[self.chromosome_position_base_pair_one] = self.base_pair_representation[0]
+        mappings_per_chromosome[self.chromosome_position_base_pair_two] = self.base_pair_representation[1]
+        mappings_per_chromosome[self.chromosome_position_base_pair_three] = self.base_pair_representation[2]
+
         return mappings_per_chromosome
     
     def interpret_SNV_type(self, position, var_nucleotide):
@@ -70,8 +74,6 @@ class Codon(object):
         return "".join("g."+_stringified_list[i] if i+1 == len(_stringified_list) else "g."+_stringified_list[i]+", " for i in range(len(_stringified_list)))
 
     def __init__(self, _mappings, _gencode_transcription_id, _uniprot_ac):
-        self.mappings = list()
-        self.mapping_ids = list()
         self.gencode_transcription_id = _gencode_transcription_id
         self.uniprot_ac = _uniprot_ac
         self.strand = str()
@@ -81,57 +83,65 @@ class Codon(object):
         self.chr = str()
         self.regions = tuple()  
         self.cDNA_position_range = tuple()
+        self.chromosome_position_base_pair_one = int()
+        self.chromosome_position_base_pair_two = int()
+        self.chromosome_position_base_pair_three = int()
         
         # check the mappings cover exactly 3 mappings (thus represent a codon)
         if len(_mappings) != 3:
             raise MalformedCodonException("Malformed codon mapping: Expected exactly 3 mappings but got '"+str(len(_mappings))+"'.")
         
         # sort the mappings
-        self.mappings = sorted(_mappings, key=lambda k: k.codon_base_pair_position)
-        self.mapping_ids = [self.mappings[0].id, self.mappings[1].id, self.mappings[2].id, ]
+        _mappings = sorted(_mappings, key=lambda k: k.codon_base_pair_position)
+        _mapping_ids = [_mappings[0].id, _mappings[1].id, _mappings[2].id, ]
         
         # double check the mapping for gene ids
-        if not(self.mappings[0].gene_id == self.mappings[1].gene_id == self.mappings[2].gene_id):
-            raise MalformedCodonException("Malformed codon mapping: The gene ids are not the same for mapping ids: '"+str(self.mapping_ids)+"'.")
+        if not(_mappings[0].gene_id == _mappings[1].gene_id == _mappings[2].gene_id):
+            raise MalformedCodonException("Malformed codon mapping: The gene ids are not the same for mapping ids: '"+str(_mapping_ids)+"'.")
         
         # double check the mapping for gene ids
-        if not(self.mappings[0].protein_id == self.mappings[1].protein_id == self.mappings[2].protein_id):
-            raise MalformedCodonException("Malformed codon mapping: The protein ids are not the same for mapping ids: '"+str(self.mapping_ids)+"'.")
+        if not(_mappings[0].protein_id == _mappings[1].protein_id == _mappings[2].protein_id):
+            raise MalformedCodonException("Malformed codon mapping: The protein ids are not the same for mapping ids: '"+str(_mapping_ids)+"'.")
         
         # double check the mapping for amino_acid_residue
-        if not(self.mappings[0].amino_acid_residue == self.mappings[1].amino_acid_residue == self.mappings[2].amino_acid_residue):
-            raise MalformedCodonException("Malformed codon mapping: The encoded amino acid residue does not correspond for mapping ids: '"+str(self.mapping_ids)+"'.")
+        if not(_mappings[0].amino_acid_residue == _mappings[1].amino_acid_residue == _mappings[2].amino_acid_residue):
+            raise MalformedCodonException("Malformed codon mapping: The encoded amino acid residue does not correspond for mapping ids: '"+str(_mapping_ids)+"'.")
         
         # double check the mapping for amino_acid_position
-        if not(self.mappings[0].amino_acid_position == self.mappings[1].amino_acid_position == self.mappings[2].amino_acid_position):
-            raise MalformedCodonException("Malformed codon mapping: The encoded amino acid position does not correspond for mapping ids: '"+str(self.mapping_ids)+"'.")
+        if not(_mappings[0].amino_acid_position == _mappings[1].amino_acid_position == _mappings[2].amino_acid_position):
+            raise MalformedCodonException("Malformed codon mapping: The encoded amino acid position does not correspond for mapping ids: '"+str(_mapping_ids)+"'.")
         
         # double check the mapping for chromosome
-        if not(self.mappings[0].chromosome == self.mappings[1].chromosome == self.mappings[2].chromosome):
-            raise MalformedCodonException("Malformed codon mapping: The chromosome does not correspond for mapping ids: '"+str(self.mapping_ids)+"'.")
+        if not(_mappings[0].chromosome == _mappings[1].chromosome == _mappings[2].chromosome):
+            raise MalformedCodonException("Malformed codon mapping: The chromosome does not correspond for mapping ids: '"+str(_mapping_ids)+"'.")
         
         # double check the mapping for strand
-        if not(self.mappings[0].strand == self.mappings[1].strand == self.mappings[2].strand):
-            raise MalformedCodonException("Malformed codon mapping: The strand does not correspond for mapping ids: '"+str(self.mapping_ids)+"'.")
+        if not(_mappings[0].strand == _mappings[1].strand == _mappings[2].strand):
+            raise MalformedCodonException("Malformed codon mapping: The strand does not correspond for mapping ids: '"+str(_mapping_ids)+"'.")
         
         # double check the cDNA positions for the mappins
-        if not(self.mappings[0].cDNA_position < self.mappings[1].cDNA_position < self.mappings[2].cDNA_position):
-            raise MalformedCodonException("Malformed codon mapping: The cDNA positions do not agree follow in order for mapping ids: '"+str(self.mapping_ids)+"'.")
+        if not(_mappings[0].cDNA_position < _mappings[1].cDNA_position < _mappings[2].cDNA_position):
+            raise MalformedCodonException("Malformed codon mapping: The cDNA positions do not agree follow in order for mapping ids: '"+str(_mapping_ids)+"'.")
         
         # set the checked values
-        self.strand = self.mappings[0].strand
-        self.amino_acid_residue = self.mappings[0].amino_acid_residue
-        self.amino_acid_position = self.mappings[0].amino_acid_position
-        self.chr = self.mappings[0].chromosome
-        self.cDNA_position_range = [self.mappings[0].cDNA_position, self.mappings[1].cDNA_position, self.mappings[2].cDNA_position,]
+        self.strand = _mappings[0].strand
+        self.amino_acid_residue = _mappings[0].amino_acid_residue
+        self.amino_acid_position = _mappings[0].amino_acid_position
+        self.chr = _mappings[0].chromosome
+        self.cDNA_position_range = [_mappings[0].cDNA_position, _mappings[1].cDNA_position, _mappings[2].cDNA_position,]
         
         # check if the codon corresponds to the base pairs
-        self.base_pair_representation = self.mappings[0].base_pair+self.mappings[1].base_pair+self.mappings[2].base_pair
-        if not(self.mappings[0].codon == self.mappings[1].codon == self.mappings[2].codon == self.base_pair_representation):
-            raise MalformedCodonException("Malformed codon mapping: The codons and base pairs of the mapping pair is malformed for mapping ids: '"+str(self.mapping_ids)+"'.")
+        self.base_pair_representation = _mappings[0].base_pair+_mappings[1].base_pair+_mappings[2].base_pair
+        if not(_mappings[0].codon == _mappings[1].codon == _mappings[2].codon == self.base_pair_representation):
+            raise MalformedCodonException("Malformed codon mapping: The codons and base pairs of the mapping pair is malformed for mapping ids: '"+str(_mapping_ids)+"'.")
+        
+        # Add the chromosome positions for each basepair
+        self.chromosome_position_base_pair_one = _mappings[0].chromosome_position
+        self.chromosome_position_base_pair_two = _mappings[1].chromosome_position
+        self.chromosome_position_base_pair_three = _mappings[2].chromosome_position
         
         # Set the regions the same way as we set the regions in a gene_region
-        self.regions = list(convertListOfIntegerToRanges(sorted([x.chromosome_position for x in self.mappings])))
+        self.regions = list(convertListOfIntegerToRanges([self.chromosome_position_base_pair_one, self.chromosome_position_base_pair_two, self.chromosome_position_base_pair_three]))
     
     def __repr__(self):
         return "<Codon(representation='%s', amino_acid_residue='%s', chr='%s', chr_positions='%s', strand='%s')>" % (
