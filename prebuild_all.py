c32108ffd1c311296b3b9a0a4f4fd05dc0f45630
Coos Baakman <c.baakman@radboudumc.nl>
2018-08-27 11:42:22 +0200

    initialize metadomains before running prebuilt initialization

--- prebuild_all.py
+++ prebuild_all.py
@@ -5,7 +5,7 @@ from time import sleep
 
 import metadome.default_settings as settings
 from metadome.application import app, celery
-from metadome.tasks import create_prebuild_visualization
+from metadome.tasks import create_prebuild_visualization, initialize_metadomain
 from metadome.domain.repositories import GeneRepository
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
@@ -19,39 +19,53 @@ def not_created(transcript_id):
     return not os.path.isfile(path)
 
 
-def monitor(results):
+def monitor(input_results):
     count_success = 0
     count_failure = 0
-    while len(results) > 0:
+    while len(input_results) > 0:
         waiting = {}
         count_running = 0
-        for id_ in results:
+        for id_ in input_results:
 
-            if results[id_].status == 'FAILURE':
+            if input_results[id_].status in ['FAILURE', 'REVOKED']:
                 count_failure += 1
 
-                _log.error("{}: {}".format(id_, results[id_].traceback))
+                _log.error("{}: {}".format(id_, input_results[id_].traceback))
 
-            elif results[id_].status == 'SUCCESS':
+            elif input_results[id_].status == 'SUCCESS':
                 count_success += 1
 
                 try:
-                    _log.info("{}: {}".format(id_,results[id_].get()))
+                    _log.info("{}: {}".format(id_, input_results[id_].get()))
                 except:
                     _log.error(traceback.format_exc())
             else:
-                if results[id_].status == 'STARTED':
+                if input_results[id_].status == 'STARTED':
                     count_running += 1
 
-                waiting[id_] = results[id_]
+                waiting[id_] = input_results[id_]
 
         _log.info("failure: {}, success: {}, waiting: {} (of which {} running)"
                   .format(count_failure, count_success,
                           len(waiting), count_running))
-        results = waiting
+        input_results = waiting
         sleep(10)
 
     _log.debug("all done, stopping..")
+
+
+_log.debug("submitting metadomain jobs")
+results = {domain_id: initialize_metadomain.delay(domain_id)
+           for domain_id in os.listdir(settings.METADOMAIN_DIR)}
+
+_log.debug("waiting for results")
+try:
+    monitor(results)
+except:
+    _log.debug("revoking all jobs")
+    for result in results.values():
+        result.revoke()
+    raise
 
 
 _log.debug("getting transcript ids")
@@ -61,7 +75,7 @@ with app.app_context():
     transcript_ids = filter(not_created,
                             [transcript.gencode_transcription_id for transcript in transcripts])
 
-_log.debug("submitting jobs")
+_log.debug("submitting visualization jobs")
 results = {transcript_id: create_prebuild_visualization.delay(transcript_id)
            for transcript_id in transcript_ids}
 
