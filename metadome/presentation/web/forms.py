20d34df8eb26f427d9861707d3c335ef47f30f0f
laurensvdwiel <laurensvdwiel@gmail.com>
2018-09-06 13:57:57 +0200

    removed unused form

--- metadome/presentation/web/forms.py
+++ metadome/presentation/web/forms.py
@@ -1,18 +1,7 @@
-from flask_wtf import Form
 from flask_wtf.form import FlaskForm
-from wtforms.fields import TextField
 from wtforms.fields.html5 import EmailField
 from wtforms.fields.simple import TextAreaField
 from wtforms import validators
-
-
-class MetaDomForm(Form):
-    entry_id = TextField(u'ID')
-    position = TextField(u'Position')
-
-    def __init__(self, allowed_extensions=None, **kwargs):
-        super(MetaDomForm, self).__init__(**kwargs)
-
 
 class SupportForm(FlaskForm):
     email = EmailField('Email',
