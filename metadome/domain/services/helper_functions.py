a2a5fd66ffb597b2ba21dbf7c277ca692b920226
Coos Baakman <cbaakman@radboudumc.nl>
2018-09-10 16:02:11 +0200

    add the is_transcript_id as a helper function

--- metadome/domain/services/helper_functions.py
+++ metadome/domain/services/helper_functions.py
@@ -1,3 +1,11 @@
+import re
+
+
+transcript_id_pattern = re.compile(r"^ENST[0-9]{11}\.[0-9]+$")
+
+def is_transcript_id(transcript_id):
+    return transcript_id_pattern.match(transcript_id) is not None
+
 
 def convertListOfIntegerToRanges(p):
     """Converts a list of integer to a list of ranges.
