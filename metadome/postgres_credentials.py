56d0e160aa472e46e216443b7800b151c30fb427
Laurens van de Wiel <laurens@F0R32DV973.local>
2022-10-20 13:36:20 +0200

    added new environment file for database

--- metadome/postgres_credentials.py
+++
@@ -1,5 +0,0 @@
-# The user needed to connect to the postgreSQL database for MetaDome
-POSTGRES_USER='metadom_user'
-# The password needed to connect to the postgreSQL database for MetaDome
-# For security reasone, please change this to something else
-POSTGRES_PASSWORD='metadome_user_pw'
